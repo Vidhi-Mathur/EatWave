538922a38c22e1bf78a5a70080e1225c4ea2aa76
Vidhi <mathurvidhi2505@gmail.com>
2024-04-27 19:39:15 +0530

    Added routes for finding menu through id and updating

--- backend/routes/restaurant-related/restaurant-route.js
+++ backend/routes/restaurant-related/restaurant-route.js
@@ -8,4 +8,10 @@ router.get('/:id', restaurantController.getRestaurantById)
 //POST /eatwave/restaurant/new
 router.post('/new', restaurantController.createRestaurant)
 
+//PATCH /eatwave/restaurant/:id
+router.patch('/:id', restaurantController.updateRestaurant)
+
+//DELETE /eatwave/restaurant/:id
+router.delete('/:id', restaurantController.deleteRestaurant)
+
 module.exports = router
