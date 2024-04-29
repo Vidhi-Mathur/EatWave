1edd46b56937b52abae73f69e09c7b173beb5bb1
Vidhi <mathurvidhi2505@gmail.com>
2024-04-29 23:21:09 +0530

    Added routes for handling orders

--- backend/routes/shared/order-route.js
+++ backend/routes/shared/order-route.js
@@ -1,19 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const orderController = require('../../controllers/shared/order-controller')
 
 //POST /eatwave/order/place
-router.post('/place')
+router.post('/place', orderController.placeOrder)
 
 //GET /eatwave/order/:id
-router.get('/:id')
+router.get('/:id', orderController.getOrderById)
 
 //PATCH /eatwave/order/status/:id
-router.patch('/status/:id')
+router.patch('/status/:id', orderController.updateOrderStatus)
 
-//GET /eatwave/order/history/:id
-router.get('/history/:id')
+//GET /eatwave/order/user_history/:id
+router.get('/user_history/:id', orderController.getUserOrderHistory)
+
+//GET /eavtwave/order/restaurant_history/:id
+router.get('/restaurant_history/:id', orderController.getRestaurantOrderHistory)
 
 //DELETE /eatwave/cancel/:id
-router.delete('/cancel/:id')
+router.delete('/cancel/:id', orderController.cancelOrder)
 
 module.exports = router
