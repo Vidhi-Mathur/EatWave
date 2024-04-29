d5aea5cb632582a5b3b969ebf3ff82acb2fe1ae4
Vidhi <mathurvidhi2505@gmail.com>
2024-04-29 23:19:19 +0530

    Added ref to 'Order' for order history access

--- backend/models/user-related/user-model.js
+++ backend/models/user-related/user-model.js
@@ -14,7 +14,12 @@ const user = new schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    pastOrders: [{
+        type: mongoose.Schema.ObjectId,
+        ref: 'Order',
+        default: null
+    }]
 })
 
 module.exports = mongoose.model('User', user)
