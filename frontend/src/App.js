9a0196e96d729aafa1f14f68ce1f2127ac53efa4
Vidhi <mathurvidhi2505@gmail.com>
2024-04-30 23:24:18 +0530

    Added routes to Signup and Terms and Conditions Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import HomePage from './components/UI/HomePage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/Pages/HomePage';
+import SignupPage from './components/Pages/SignupPage';
+import TermsAndConditions from './components/Pages/TermsAndConditionsPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        </Routes>
       </Router>
     </div>
   );
 }
 
-export default App;
+export default App
