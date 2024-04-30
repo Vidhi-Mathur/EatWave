03667370f1480cd63e3aef60859cc07254db9ae9
Vidhi <mathurvidhi2505@gmail.com>
2024-04-30 23:21:31 +0530

    Default Page

---
+++ frontend/src/components/Pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../UI/Header";
+import backgroundImage from "../../assets/EatWaveBg.jpeg";
+
+const HomePage = ({children, customisedImageUrl}) => {
+  return (
+    <div className="relative">
+      <Header />
+      <div
+        className="fixed top-0 left-0 w-full h-full bg-cover bg-no-repeat bg-center z-0"
+        style={{ backgroundImage: `url(${customisedImageUrl || backgroundImage})` }}
+      />
+      {/* Appers above background image */}
+      <div className="relative z-10">{children}</div>
+    </div>
+  );
+};
+
+export default HomePage;
