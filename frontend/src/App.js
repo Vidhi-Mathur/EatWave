66a807f9cd8585e908168458c0edc0abd9115fb3
Vidhi <mathurvidhi2505@gmail.com>
2024-05-03 23:10:24 +0530

    Added routes for login, cart and restaurant details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/Pages/HomePage';
-import SignupPage from './components/Pages/SignupPage';
+import AuthFormPage from './components/Pages/AuthPage.js';
 import TermsAndConditions from './components/Pages/TermsAndConditionsPage';
+import RestaurantDetailPage from './components/Pages/RestaurantDetailPage.js';
+import { AuthCtxProvider } from './store/Auth-Context.js';
+import { MenuCtxProvider } from './store/Menu-Context.js';
+import CartPage from './components/Pages/CartPage.js';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <AuthCtxProvider>
+          <MenuCtxProvider>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/signup" element={<AuthFormPage signup/>} />
+          <Route path="/login" element={<AuthFormPage signup={false}/>} />
           <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+          <Route path="/restaurant/:id" element={<RestaurantDetailPage />} />
+          <Route path='/restuarant/add-restaurant' element={<RestaurantDetailPage />}/>
+          <Route path='/cart' element={<CartPage />}/>
         </Routes>
+          </MenuCtxProvider>
+        </AuthCtxProvider>
       </Router>
     </div>
   );
