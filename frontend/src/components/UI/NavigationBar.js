4f5d6afa9aea965874e9d4e7a0a7e594c9be7208
Vidhi <mathurvidhi2505@gmail.com>
2024-05-03 23:07:47 +0530

    Managed routes exclusively for signup/ login

--- frontend/src/components/UI/NavigationBar.js
+++ frontend/src/components/UI/NavigationBar.js
@@ -6,17 +6,21 @@ import LoginIcon from '@mui/icons-material/Login';
 import ShoppingCartSharpIcon from '@mui/icons-material/ShoppingCartSharp';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
-import { useState } from "react";
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import { useContext, useState } from "react";
+import { AuthContext } from "../../store/Auth-Context";
 
 
 const NavLinks = (props) => {
+    const { isAuthenticated } = useContext(AuthContext)
     return (
         <>
         <NavLink to="/search" className="mr-16"><SearchSharpIcon /> Search</NavLink>
         <NavLink to="/offers" className="mr-16"><LocalOfferOutlinedIcon /> Offers</NavLink>
         <NavLink to="/help" className="mr-16"><HelpOutlineSharpIcon /> Help</NavLink> 
         <NavLink to="/signup" className="mr-16"><LoginIcon /> Signup</NavLink>
-        <NavLink to="/cart" className="mr-16"><ShoppingCartSharpIcon /> Cart</NavLink>
+        {isAuthenticated && <NavLink to="/cart" className="mr-16"><ShoppingCartSharpIcon /> Cart</NavLink>}
+        {isAuthenticated && <NavLink to="/restaurant/add-restaurant" className="mr-16"><RestaurantIcon /> Add Restaurant</NavLink>}
         </>
     )
 }
