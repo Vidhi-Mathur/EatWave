5e44c9329e4d5214313b401b799618240eb816d6
Vidhi <mathurvidhi2505@gmail.com>
2024-05-04 22:46:28 +0530

    Renders Cart summary

--- frontend/src/components/Pages/CartPage.js
+++ frontend/src/components/Pages/CartPage.js
@@ -14,53 +14,34 @@ const CartPage = () => {
   return (
     <Layout>
       <Content>
-        <h2 className="text-2xl font-bold mb-4">Shopping Cart</h2>
+        <h2 className="text-2xl font-bold mb-4 pt-2 pl-2">Shopping Cart</h2>
         {items.map((item) => {
           return (
-            <>
-              <div
-                key={item._id}
-                className="flex items-center justify-between bg-white rounded-lg shadow-md p-4 mb-4"
-              >
-                <div>
-                  <h3 className="text-lg font-semibold">{item.name}</h3>
-                  <p className="text-gray-600">Quantity: {item.quantity}</p>
-                  <p className="text-gray-800 mt-2">${item.price}</p>
-                </div>
-                <div>
-                  {item.quantity === 0 ? (
-                    <button
-                      onClick={() => addToCart({ itemId: item._id })}
-                      className="bg-orange-100 px-4 py-3 rounded-md"
-                    >
-                      ADD
-                    </button>
-                  ) : (
-                    <div className="flex items-center">
-                      <button
-                        onClick={() => removeFromCart({ itemId: item._id })}
-                        className="bg-orange-100 px-3 py-2 rounded-md"
-                      >
-                        <RemoveIcon />
-                      </button>
-                      <span className="px-4">{item.quantity}</span>
-                      <button
-                        onClick={() => addToCart({ itemId: item._id })}
-                        className="bg-orange-100 px-3 py-2 rounded-md"
-                      >
-                        <AddIcon />
-                      </button>
-                    </div>
-                  )}
-                </div>
+            <div key={item.id} className="flex items-center justify-between bg-white rounded-lg shadow-md p-4 mb-4">
+              <div>
+                <h3 className="text-lg font-semibold">{item.name}</h3>
+                <p className="text-gray-600">Quantity: {item.quantity}</p>
+                <p className="text-gray-800 mt-2">${item.price}</p>
               </div>
-            </>
+              <div>
+                {item.quantity === 0 ? (
+                  <button onClick={() => addToCart({ itemId: item.id, name: item.name, price: item.price})} className="bg-orange-100 px-4 py-3 rounded-md">ADD</button>) : (
+                  <div className="flex items-center">
+                    <button onClick={() => removeFromCart({ itemId: item.id })} className="bg-orange-100 px-3 py-2 rounded-md">
+                      <RemoveIcon />
+                    </button>
+                    <span className="px-4">{item.quantity}</span>
+                    <button onClick={() => addToCart({ itemId: item.id, name: item.name, price: item.price })} className="bg-orange-100 px-3 py-2 rounded-md">
+                      <AddIcon />
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
           );
         })}
-        <div className="mt-8">
-          <h3 className="text-lg font-semibold">
-            Total: ${totalPrice.toFixed(2)}
-          </h3>
+        <div className="mt-8 pb-2 pl-2">
+          <h3 className="text-lg font-semibold">Total: ${totalPrice.toFixed(2)}</h3>
         </div>
       </Content>
     </Layout>
