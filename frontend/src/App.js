0987efc6707d59762506efcb1d7f818c96ea3bf2
Vidhi <mathurvidhi2505@gmail.com>
2024-05-04 22:54:10 +0530

    C:/Program Files/Git/cart added, displayed only when authenticated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/Pages/HomePage';
 import AuthFormPage from './components/Pages/AuthPage.js';
 import TermsAndConditions from './components/Pages/TermsAndConditionsPage';
 import RestaurantDetailPage from './components/Pages/RestaurantDetailPage.js';
-import { AuthCtxProvider } from './store/Auth-Context.js';
+import { AuthContext, AuthCtxProvider } from './store/Auth-Context.js';
 import { MenuCtxProvider } from './store/Menu-Context.js';
 import CartPage from './components/Pages/CartPage.js';
 
 function App() {
+  const { isAuthenticated }= useContext(AuthContext)
   return (
     <div className="App">
       <Router>
@@ -20,8 +21,8 @@ function App() {
           <Route path="/login" element={<AuthFormPage signup={false}/>} />
           <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
           <Route path="/restaurant/:id" element={<RestaurantDetailPage />} />
-          <Route path='/restuarant/add-restaurant' element={<RestaurantDetailPage />}/>
-          <Route path='/cart' element={<CartPage />}/>
+          <Route path='/restuarant/add-restaurant' element={<RestaurantDetailPage />} />
+          {isAuthenticated && <Route path='/cart' element={<CartPage />} />}
         </Routes>
           </MenuCtxProvider>
         </AuthCtxProvider>
