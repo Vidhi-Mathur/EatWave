581a8800f0275f1c44b8c19efbae17cf64b73208
Vidhi <mathurvidhi2505@gmail.com>
2024-05-07 10:06:29 +0530

    Fixed authentication issues for '/cart', and '/order', added Error Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,31 +4,34 @@ import HomePage from './components/Pages/HomePage';
 import AuthFormPage from './components/Pages/AuthPage.js';
 import TermsAndConditions from './components/Pages/TermsAndConditionsPage';
 import RestaurantDetailPage from './components/Pages/RestaurantDetailPage.js';
-import { AuthContext, AuthCtxProvider } from './store/Auth-Context.js';
-import { MenuCtxProvider } from './store/Menu-Context.js';
+import { AuthContext } from './store/Auth-Context.js';
 import CartPage from './components/Pages/CartPage.js';
+import { OrderPage } from './components/Pages/OrderPage.js';
+import { ErrorPage } from './components/Pages/ErrorPage.js';
 
 function App() {
-  const { isAuthenticated }= useContext(AuthContext)
+  const { isAuthenticated } = useContext(AuthContext);
   return (
     <div className="App">
       <Router>
-        <AuthCtxProvider>
-          <MenuCtxProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<AuthFormPage signup/>} />
-          <Route path="/login" element={<AuthFormPage signup={false}/>} />
-          <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-          <Route path="/restaurant/:id" element={<RestaurantDetailPage />} />
-          <Route path='/restuarant/add-restaurant' element={<RestaurantDetailPage />} />
-          {isAuthenticated && <Route path='/cart' element={<CartPage />} />}
-        </Routes>
-          </MenuCtxProvider>
-        </AuthCtxProvider>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/signup" element={<AuthFormPage signup />} />
+              <Route path="/login" element={<AuthFormPage signup={false} />} />
+              <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+              <Route path="/restaurant/:restaurantId" element={<RestaurantDetailPage />} />
+              <Route path='/restaurant/add-restaurant' element={<RestaurantDetailPage />} />
+              {isAuthenticated && (
+                <>
+                  <Route path='/cart' element={<CartPage />} />
+                  <Route path='/order' element={<OrderPage />} />
+                </>
+              )}
+              <Route path="*" element={<ErrorPage />}/>
+            </Routes>
       </Router>
     </div>
   );
 }
 
-export default App
+export default App;
