0e5e1677cd0c8c74a717d8047449bd072d960606
Vidhi <mathurvidhi2505@gmail.com>
2024-05-07 09:59:11 +0530

    404 error page

---
+++ frontend/src/components/Pages/ErrorPage.js
@@ -0,0 +1,18 @@
+import Card from "../UI/Card";
+import Layout from "../UI/Layout";
+import { Link } from "react-router-dom"
+
+export const ErrorPage = () => {
+    return (
+        <Layout>
+            <Card>
+            <div className="flex flex-col items-center justify-center h-full">
+                <h1 className="text-4xl font-bold text-center text-gray-900 mb-4">Oops!</h1>
+                <p className="text-lg text-center text-gray-700 mb-4">Looks Like You Took a Wrong Turn</p>
+                <p className="text-lg text-center text-gray-700 mb-8">Let's Redirect You to Deliciousness!</p>
+                <Link to="/" className="bg-orange-400 hover:bg-orange-600 text-white font-semibold py-2 px-6 rounded-lg transition-colors duration-300 ease-in-out">Back to Food</Link>
+            </div>
+            </Card>
+        </Layout>
+    );
+};
