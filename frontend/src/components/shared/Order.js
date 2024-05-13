a91d5e8f1fd8e4a82d1a6cb44b330d6f9ee90def
Vidhi <mathurvidhi2505@gmail.com>
2024-05-13 22:52:10 +0530

    Managing error state

--- frontend/src/components/shared/Order.js
+++ frontend/src/components/shared/Order.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { MenuContext } from "../../store/Menu-Context"
 import Card from "../UI/Card"
 import { AuthContext } from "../../store/Auth-Context";
@@ -9,6 +9,7 @@ export const Order = () => {
     const publishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
     const { restaurant, items } = useContext(MenuContext);
     const { token, setToken } = useContext(AuthContext)
+    const [error, setError] = useState(null)
     const orderHandler = async (e) => {
         e.preventDefault();
         const form = new FormData(e.target);
@@ -27,8 +28,11 @@ export const Order = () => {
                 },
                 body: JSON.stringify(paymentOrder)
             });
-            if (!paymentResponse.ok) throw new Error('Failed to make payment');
             const paymentResult = await paymentResponse.json();
+            if (!paymentResponse.ok){
+                setError(paymentResult.message)
+                return;
+            }
             const stripe = await loadStripe(publishableKey);
             // Redirect to Stripe checkout for payment
             await stripe.redirectToCheckout({
@@ -50,38 +54,42 @@ export const Order = () => {
                 },
                 body: JSON.stringify(order)
             });
-            if (!orderResponse.ok) throw new Error('Failed to place order');
             const orderResult = await orderResponse.json(); 
+            if (!orderResponse.ok){
+                setError(orderResult.message)
+                return;
+            }
             setToken(orderResult.token);      
             return orderResult;
         } catch (err) {
-            console.log(err);
+            setError("Placing order failed, try again later")
         }
     };
 
     return (
         <Layout>
             <Card>
-    <form className="space-y-4" onSubmit={orderHandler}>
-        <div>
-            <label htmlFor="street" className="block text-sm font-medium text-gray-700">Street</label>
-            <input type="text" name="street" placeholder="Street" required className="w-full pl-3 pr-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange-400" />
-        </div>
-        <div>
-            <label htmlFor="city" className="block text-sm font-medium text-gray-700">City</label>
-            <input type="text" name="city" placeholder="City" required className="w-full pl-3 pr-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange-400" />
-        </div>
-        <div>
-            <label htmlFor="state" className="block text-sm font-medium text-gray-700">State</label>
-            <input type="text" name="state" placeholder="State" required className="w-full pl-3 pr-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange-400" />
-        </div>
-        <div>
-            <label htmlFor="postalCode" className="block text-sm font-medium text-gray-700">Postal Code</label>
-            <input type="text" name="postalCode" placeholder="Postal Code" required className="w-full pl-3 pr-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange-400" />
-        </div>
-    <button type="submit" className="w-full py-2 px-4 bg-orange-400 text-white rounded-md hover:bg-orange-500 focus:outline-none focus:bg-orange-500">Pay and Order</button>
-    </form>
-        </Card>
+                {error && <p className="text-red-500 text-center m-4">{error}</p>}
+                <form className="space-y-4" onSubmit={orderHandler}>
+                    <div>
+                        <label htmlFor="street" className="block text-sm font-medium text-gray-700">Street</label>
+                        <input type="text" name="street" placeholder="Street" required className="w-full pl-3 pr-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange-400" />
+                    </div>
+                    <div>
+                        <label htmlFor="city" className="block text-sm font-medium text-gray-700">City</label>
+                        <input type="text" name="city" placeholder="City" required className="w-full pl-3 pr-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange-400" />
+                    </div>
+                    <div>
+                        <label htmlFor="state" className="block text-sm font-medium text-gray-700">State</label>
+                        <input type="text" name="state" placeholder="State" required className="w-full pl-3 pr-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange-400" />
+                    </div>
+                    <div>
+                        <label htmlFor="postalCode" className="block text-sm font-medium text-gray-700">Postal Code</label>
+                        <input type="text" name="postalCode" placeholder="Postal Code" required className="w-full pl-3 pr-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-orange-400" />
+                    </div>
+                <button type="submit" className="w-full py-2 px-4 bg-orange-400 text-white rounded-md hover:bg-orange-500 focus:outline-none focus:bg-orange-500">Pay and Order</button>
+                </form>
+            </Card>
         </Layout>
     )
 }
