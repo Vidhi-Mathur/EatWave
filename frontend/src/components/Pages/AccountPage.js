0df52b50f54249bd4823606db8c85b9f98b4bb01
Vidhi <mathurvidhi2505@gmail.com>
2024-05-24 22:51:38 +0530

    Added styling

--- frontend/src/components/Pages/AccountPage.js
+++ frontend/src/components/Pages/AccountPage.js
@@ -8,33 +8,33 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 export const AccountPage = () => {
     return (
         <Layout>
-            <Content>
+          <Content>
             <div className="container mx-auto p-4">
-      <main className="flex">
-        <nav className="w-1/4 p-4">
-          <ul>
-            <li className="mb-2">
-              <NavLink to="/my-account/orders" className="flex items-center p-2 hover:bg-gray-100 rounded">
-                <span className="ml-2"><ShoppingBagIcon /> Orders</span>
-              </NavLink>
-            </li>
-            <li className="mb-2">
-              <NavLink to="/my-account/favorites" className="flex items-center p-2 hover:bg-gray-100 rounded">
-                <span className="ml-2"><FavoriteIcon /> Favorites</span>
-              </NavLink>
-            </li>
-            <li className="mb-2">
-              <NavLink to="/my-account/addresses" className="flex items-center p-2 hover:bg-gray-100 rounded">
-                <span className="ml-2"><LocationOnIcon /> Addresses</span>
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-        <section className="w-3/4 p-4">
-          <Outlet />
-        </section>
-      </main>
-    </div>
+              <main className="flex">
+                <nav className="w-1/4 p-4">
+                  <ul>
+                    <li className="mb-2">
+                      <NavLink to="/my-account/orders" className="flex items-center p-2 hover:bg-gray-100 rounded">
+                        <span className="ml-2"><ShoppingBagIcon /> Orders</span>
+                      </NavLink>
+                    </li>
+                    <li className="mb-2">
+                      <NavLink to="/my-account/favorites" className="flex items-center p-2 hover:bg-gray-100 rounded">
+                        <span className="ml-2"><FavoriteIcon /> Favorites</span>
+                      </NavLink>
+                    </li>
+                    <li className="mb-2">
+                      <NavLink to="/my-account/addresses" className="flex items-center p-2 hover:bg-gray-100 rounded">
+                        <span className="ml-2"><LocationOnIcon /> Addresses</span>
+                      </NavLink>
+                    </li>
+                  </ul>
+                </nav>
+                <section className="w-3/4 p-4">
+                  <Outlet />
+                </section>
+              </main>
+              </div>
             </Content>
         </Layout>
     )
