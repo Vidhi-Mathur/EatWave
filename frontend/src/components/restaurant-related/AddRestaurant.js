971b5784016ea369e417cb4bfa25bbc8d05b2de9
Vidhi <mathurvidhi2505@gmail.com>
2024-05-28 22:35:29 +0530

    Select All button works, removed unecessary fields and menu items can be added

--- frontend/src/components/restaurant-related/AddRestaurant.js
+++ frontend/src/components/restaurant-related/AddRestaurant.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { StepIndicator } from '../UI/StepIndicator';
+import { AddMenuItems } from './AddMenuItems';
 
+let weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+let steps = ['Restaurant Information', 'Restaurant Documents', 'Menu Setup'];
 export const AddRestaurant = () => {
-  const steps = ['Restaurant Information', 'Restaurant Documents', 'Menu Setup'];
   const [currentStep, setCurrentStep] = useState(0);
   const [formData, setFormData] = useState({ working_days: [] });
+  const [menuItems, setMenuItems] = useState([])
 
   const nextHandler = () => {
     if (currentStep < steps.length - 1) {
@@ -28,12 +31,42 @@ export const AddRestaurant = () => {
     } 
     else setFormData({ ...formData, [name]: value })
   };
-  
+
+  const selectAllHandler = () => {
+    setFormData(prevState => {
+      if (prevState.working_days.length === weekdays.length) {
+        return { ...prevState, working_days: [] };
+      } else {
+        return { ...prevState, working_days: weekdays };
+      }
+    });
+  };
   
   const submitHandler = (e) => {
     e.preventDefault();
     console.log({ ...formData });
   };
+
+  const changeMenuItemHandler = (e, idx) => {
+    const { name, value } = e.target;
+    const updatedItems = [...menuItems];
+    const field = name.split('-')[0];
+    updatedItems[idx][field] = value;
+    setMenuItems(updatedItems);
+  }
+
+  const removeMenuItemHandler = (idx) => {
+    const updatedItems = menuItems.filter((_,i) => i !== idx)
+    setMenuItems(updatedItems)
+  }
+
+  const addMenuItemHandler = () => {
+    setMenuItems(prevMenuItems => [...prevMenuItems, generateMenuItems()])
+  }
+  
+  const generateMenuItems = () => {
+    return { name: '', description: '', price: '', tags: []}
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center mt-4 mb-4">
@@ -49,6 +82,7 @@ export const AddRestaurant = () => {
                     <h1 className="text-md font-semibold mb-2">Basic Details</h1>
                     <input type="text" name="owner" className="border p-2 w-full mb-4" placeholder="Owner's Full Name" />
                     <input type="text" name="restaurant" className="border p-2 w-full mb-4" placeholder="Restaurant's Name"  />
+                    {/* Later - ToDo add location*/}
                     <button type="button" className="text-orange-500 mb-4">Add Restaurant Location</button>
                   </div>
                   <div className="border rounded p-4 shadow mb-6">
@@ -66,12 +100,14 @@ export const AddRestaurant = () => {
                     </div>
                   </div>
                   <div className="border rounded p-4 shadow mb-6">
-                    <h1 className="text-md font-semibold mb-2">Working Days</h1>
-                    <button type="button" className="text-orange-500 mb-4">Select All</button>
+                    <div className='flex items-center justify-between mb-2'>
+                      <h1 className="text-md font-semibold">Working Days</h1>
+                      <button type="button" className="text-orange-500" onClick={selectAllHandler}>Select All</button>
+                    </div>
                     <div className="grid grid-cols-2 gap-2">
-                      {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map(day => (
+                      {weekdays.map(day => (
                         <div key={day} className="flex items-center">
-                          <input type="checkbox" id={day.toLowerCase()} name="working_days" value={day} className="mr-2" />
+                          <input type="checkbox" id={day.toLowerCase()} checked={formData.working_days.includes(day)} name="working_days" value={day} className="mr-2" />
                           <label htmlFor={day.toLowerCase()}>{day}</label>
                         </div>
                       ))}
@@ -80,6 +116,7 @@ export const AddRestaurant = () => {
                   <div className="border rounded p-4 shadow mb-6">
                     <h1 className="text-md font-semibold mb-2">Opening and Closing Time</h1>
                     <div className="mt-4">
+                      {/* Later - ToDo make additional slots available*/}
                       <label className="block mb-2">Opening Time</label>
                       <input type="time" name="opening_time" className="border p-2 w-full mb-4" />
                       <label className="block mb-2">Closing Time</label>
@@ -93,18 +130,8 @@ export const AddRestaurant = () => {
                 <>
                   <h1 className='text-2xl font-bold'>Restaurant Documents</h1>
                   <div className="border rounded p-4 shadow mb-6">
-                    <h1 className="text-md font-semibold mb-2">Enter PAN and GSTIN details</h1>
-                    <input type="text" name="pan" className="border p-2 w-full mb-4" placeholder="Business/Owner PAN" />
-                    <input type="text" name="gst" className="border p-2 w-full mb-4" placeholder="GSTIN" />
-                    <div className="flex items-center">
-                      <input type="checkbox" id="has_gst" name="gst_option" value="gst" className="mr-2" />
-                      <label htmlFor="has_gst">I don't have a GST number</label>
-                    </div>
-                  </div>
-                  <div className="border rounded p-4 shadow mb-6">
                     <h1 className="text-md font-semibold mb-2">Official Bank Details</h1>
                     <p className='mb-2'>Payments from EatWave will be credited here</p>
-                    <input type="text" name="ifsc" className="border p-2 w-full mb-4" placeholder="Bank IFSC code" />
                     <input type="text" name="account" className="border p-2 w-full mb-4" placeholder="Bank Account number" />
                   </div>
                   <div className="border rounded p-4 shadow">
@@ -128,17 +155,17 @@ export const AddRestaurant = () => {
                     </div>
                     <hr className="my-4" />
                     <div>
+                      {/* Later ToDo - Add cuisines */}
                       <h2 className="text-md font-semibold mb-2">Add cuisines that you serve</h2>
                       <button type="button" className="text-orange-500 mb-4">+ Add More</button>
                     </div>
                   </div>
                   <div className="border rounded p-4 shadow mb-6">
-                    <h3 className="text-md font-semibold mb-2">Cost for two</h3>
-                    <input type="number" id="cost" name="costForTwo" placeholder="Rs." className="border p-2 w-full mb-4" />
-                  </div>
-                  <div className="border rounded p-4 shadow mb-6">
                   <h3 className="text-md font-semibold mb-2">Add your menu</h3>
-                  <input type="file" name="menu" className="border p-2 w-full mb-4" />
+                  {menuItems.map((item, idx) => (
+                    <AddMenuItems key={idx} idx={idx} item={item} changeMenuItem={changeMenuItemHandler} removeMenuItem={removeMenuItemHandler}/>
+                  ))}
+                  <button type="button" className="text-orange-500 mb-4" onClick={addMenuItemHandler}>+ Add More</button>
                   </div>
                   <div className="border rounded p-4 shadow mb-6">
                   <h3 className="text-md font-semibold mb-2">Packaging Charges</h3>
