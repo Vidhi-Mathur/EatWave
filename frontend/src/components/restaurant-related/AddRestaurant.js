a5184db17d007e6c9110af30896fd392d2220e5e
Vidhi <mathurvidhi2505@gmail.com>
2024-05-29 19:23:17 +0530

    Fixed submission of form on proceed button

--- frontend/src/components/restaurant-related/AddRestaurant.js
+++ frontend/src/components/restaurant-related/AddRestaurant.js
@@ -77,115 +77,120 @@ export const AddRestaurant = () => {
           <StepIndicator steps={steps} currentStep={currentStep} />
           <div className="w-3/4 p-4">
             <form onChange={changeHandler} onSubmit={submitHandler} className="space-y-4">
-              {currentStep === 0 && (
-                <>
-                  <h1 className='text-2xl font-bold'>Restaurant Information</h1>
-                  <div className="border rounded p-4 shadow mb-6">
-                    <h1 className="text-md font-semibold mb-2">Basic Details</h1>
-                    <input type="text" name="owner" className="border p-2 w-full mb-4" placeholder="Owner's Full Name" />
-                    <input type="text" name="restaurant" className="border p-2 w-full mb-4" placeholder="Restaurant's Name"  />
-                    {/* Later - ToDo add location*/}
-                    <button type="button" className="text-orange-500 mb-4">Add Restaurant Location</button>
-                  </div>
-                  <div className="border rounded p-4 shadow mb-6">
-                    <h1 className="text-md font-semibold mb-2">Owner Contact Details</h1>
-                    <input type="email" name="email" className="border p-2 w-full mb-4" placeholder="Email Address" />
-                    <input type="text" name="phone" className="border p-2 w-full mb-4" placeholder="Phone Number" />
-                    <p>Provide your <strong>WhatsApp Number</strong> to get insights on ratings, menu etc.</p>
-                    <div className="flex items-center mt-4 mb-4">
-                      <input type="radio" id="same_whatsapp" name="whatsapp_option" value="same" className="mr-2" />
-                      <label htmlFor="same_whatsapp">My WhatsApp Number is same as above</label>
+            {[...Array(steps.length)].map((_, index) => (
+              <div key={index} className={currentStep === index ? '' : 'hidden'}>
+                {index === 0 && (
+                  <>
+                    <h1 className='text-2xl font-bold'>Restaurant Information</h1>
+                    <div className="border rounded p-4 shadow mb-6">
+                      <h1 className="text-md font-semibold mb-2">Basic Details</h1>
+                      <input type="text" name="owner" className="border p-2 w-full mb-4" placeholder="Owner's Full Name" />
+                      <input type="text" name="restaurant" className="border p-2 w-full mb-4" placeholder="Restaurant's Name"  />
+                      {/* Later - ToDo add location*/}
+                      <button type="button" className="text-orange-500 mb-4">Add Restaurant Location</button>
                     </div>
-                    <div className="flex items-center">
-                      <input type="radio" id="different_whatsapp" name="whatsapp_option" value="different" className="mr-2" />
-                      <label htmlFor="different_whatsapp">I have a different WhatsApp number</label>
+                    <div className="border rounded p-4 shadow mb-6">
+                      <h1 className="text-md font-semibold mb-2">Owner Contact Details</h1>
+                      <input type="email" name="email" className="border p-2 w-full mb-4" placeholder="Email Address" />
+                      <input type="text" name="phone" className="border p-2 w-full mb-4" placeholder="Phone Number" />
+                      <p>Provide your <strong>WhatsApp Number</strong> to get insights on ratings, menu etc.</p>
+                      <div className="flex items-center mt-4 mb-4">
+                        <input type="radio" id="same_whatsapp" name="whatsapp_option" value="same" className="mr-2" />
+                        <label htmlFor="same_whatsapp">My WhatsApp Number is same as above</label>
+                      </div>
+                      <div className="flex items-center">
+                        <input type="radio" id="different_whatsapp" name="whatsapp_option" value="different" className="mr-2" />
+                        <label htmlFor="different_whatsapp">I have a different WhatsApp number</label>
+                      </div>
                     </div>
-                  </div>
-                  <div className="border rounded p-4 shadow mb-6">
-                    <div className='flex items-center justify-between mb-2'>
-                      <h1 className="text-md font-semibold">Working Days</h1>
-                      <button type="button" className="text-orange-500" onClick={selectAllHandler}>Select All</button>
+                    <div className="border rounded p-4 shadow mb-6">
+                      <div className='flex items-center justify-between mb-2'>
+                        <h1 className="text-md font-semibold">Working Days</h1>
+                        <button type="button" className="text-orange-500" onClick={selectAllHandler}>Select All</button>
+                      </div>
+                      <div className="grid grid-cols-2 gap-2">
+                        {weekdays.map(day => (
+                          <div key={day} className="flex items-center">
+                            <input type="checkbox" id={day.toLowerCase()} checked={formData.working_days.includes(day)} name="working_days" value={day} className="mr-2" />
+                            <label htmlFor={day.toLowerCase()}>{day}</label>
+                          </div>
+                        ))}
+                      </div>
                     </div>
-                    <div className="grid grid-cols-2 gap-2">
-                      {weekdays.map(day => (
-                        <div key={day} className="flex items-center">
-                          <input type="checkbox" id={day.toLowerCase()} checked={formData.working_days.includes(day)} name="working_days" value={day} className="mr-2" />
-                          <label htmlFor={day.toLowerCase()}>{day}</label>
-                        </div>
-                      ))}
+                    <div className="border rounded p-4 shadow mb-6">
+                      <h1 className="text-md font-semibold mb-2">Opening and Closing Time</h1>
+                      <div className="mt-4">
+                        {/* Later - ToDo make additional slots available*/}
+                        <label className="block mb-2">Opening Time</label>
+                        <input type="time" name="opening_time" className="border p-2 w-full mb-4" />
+                        <label className="block mb-2">Closing Time</label>
+                        <input type="time" name="closing_time" className="border p-2 w-full mb-4" />
+                      </div>
+                      <p>Opening and Closing Time remains same on all working days</p>
                     </div>
-                  </div>
-                  <div className="border rounded p-4 shadow mb-6">
-                    <h1 className="text-md font-semibold mb-2">Opening and Closing Time</h1>
-                    <div className="mt-4">
-                      {/* Later - ToDo make additional slots available*/}
-                      <label className="block mb-2">Opening Time</label>
-                      <input type="time" name="opening_time" className="border p-2 w-full mb-4" />
-                      <label className="block mb-2">Closing Time</label>
-                      <input type="time" name="closing_time" className="border p-2 w-full mb-4" />
-                    </div>
-                    <p>Opening and Closing Time remains same on all working days</p>
-                  </div>
-                </>
-              )}
-              {currentStep === 1 && (
-                <>
-                  <h1 className='text-2xl font-bold'>Restaurant Documents</h1>
-                  <div className="border rounded p-4 shadow mb-6">
-                    <h1 className="text-md font-semibold mb-2">Official Bank Details</h1>
-                    <p className='mb-2'>Payments from EatWave will be credited here</p>
-                    <input type="text" name="account" className="border p-2 w-full mb-4" placeholder="Bank Account number" />
-                  </div>
-                  <div className="border rounded p-4 shadow">
-                    <h1 className="text-md font-semibold">FSSAI certificate</h1>
-                    <input type="number" name="fssai" className="border p-2 w-full mb-4" placeholder="FSSAI certificate number" />
-                  </div>
-                </>
-              )}
-              {currentStep === 2 && (
-                <>
-                  <h1 className='text-2xl font-bold'>Menu Setup</h1>
-                  <div className="border rounded p-4 shadow mb-6">
-                    <h1 className="text-md font-semibold mb-2">What Kind of food is on your menu?</h1>
-                    <div className="flex items-center mb-4">
-                      <input type="radio" id="veg" name="food_option" value="veg" className="mr-2" />
-                      <label htmlFor="veg">Veg only</label>
-                    </div>
-                    <div className="flex items-center">
-                      <input type="radio" id="both_food" name="food_option" value="both_food" className="mr-2" />
-                      <label htmlFor="both_food">Both Veg & Non-veg</label>
-                    </div>
-                    <hr className="my-4" />
-                    <div>
-                      {/* Later ToDo - Add cuisines */}
-                      <h2 className="text-md font-semibold mb-2">Add cuisines that you serve</h2>
-                      <button type="button" className="text-orange-500 mb-4">+ Add More</button>
-                    </div>
-                  </div>
-                  <div className="border rounded p-4 shadow mb-6">
-                  <h3 className="text-md font-semibold mb-2">Add your menu</h3>
-                  {menuItems.map((item, idx) => (
-                    <AddMenuItems key={idx} idx={idx} item={item} changeMenuItem={changeMenuItemHandler} removeMenuItem={removeMenuItemHandler}/>
-                  ))}
-                  <button type="button" className="text-orange-500 mb-4" onClick={addMenuItemHandler}>+ Add More</button>
-                  </div>
-                  <div className="border rounded p-4 shadow mb-6">
-                  <h3 className="text-md font-semibold mb-2">Packaging Charges</h3>
-                  <p className="mb-4">Not applicable on Indian Breads, packaged items, and beverages</p>
-                  <input type="number" id="packaging_cost" name="packaging_cost" placeholder="Rs." className="border p-2 w-full mb-4" />
-                  </div>
-                </>
-              )}
-              <div className="flex justify-between">
-                {currentStep > 0 && (
-                  <button type="button" onClick={backHandler} className="bg-gray-500 text-white py-2 px-4 rounded">Back</button>
+                  </>
                 )}
-                {currentStep < steps.length - 1 ? (
-                  <button type="button" onClick={nextHandler} className="bg-orange-500 text-white py-2 px-4 rounded">Proceed</button>
-                ) : (
-                  <button type="submit" className="bg-green-500 text-white py-2 px-4 rounded">Submit</button>
+                {index === 1 && (
+                  <>
+                    <h1 className='text-2xl font-bold'>Restaurant Documents</h1>
+                    <div className="border rounded p-4 shadow mb-6">
+                      <h1 className="text-md font-semibold mb-2">Official Bank Details</h1>
+                      <p className='mb-2'>Payments from EatWave will be credited here</p>
+                      <input type="text" name="account" className="border p-2 w-full mb-4" placeholder="Bank Account number" />
+                    </div>
+                    <div className="border rounded p-4 shadow">
+                      <h1 className="text-md font-semibold">FSSAI certificate</h1>
+                      <input type="number" name="fssai" className="border p-2 w-full mb-4" placeholder="FSSAI certificate number" />
+                    </div>
+                  </>
+                )}
+                {index === 2 && (
+                  <>
+                    <h1 className='text-2xl font-bold'>Menu Setup</h1>
+                    <div className="border rounded p-4 shadow mb-6">
+                      <h1 className="text-md font-semibold mb-2">What Kind of food is on your menu?</h1>
+                      <div className="flex items-center mb-4">
+                        <input type="radio" id="veg" name="food_option" value="veg" className="mr-2" />
+                        <label htmlFor="veg">Veg only</label>
+                      </div>
+                      <div className="flex items-center">
+                        <input type="radio" id="both_food" name="food_option" value="both_food" className="mr-2" />
+                        <label htmlFor="both_food">Both Veg & Non-veg</label>
+                      </div>
+                      <hr className="my-4" />
+                      <div>
+                        {/* Later ToDo - Add cuisines */}
+                        <h2 className="text-md font-semibold mb-2">Add cuisines that you serve</h2>
+                        <button type="button" className="text-orange-500 mb-4">+ Add More</button>
+                      </div>
+                    </div>
+                    <div className="border rounded p-4 shadow mb-6">
+                    <h3 className="text-md font-semibold mb-2">Add your menu</h3>
+                    {menuItems.map((item, idx) => (
+                      <AddMenuItems key={idx} idx={idx} item={item} changeMenuItem={changeMenuItemHandler} removeMenuItem={removeMenuItemHandler}/>
+                    ))}
+                    <button type="button" className="text-orange-500 mb-4" onClick={addMenuItemHandler}>+ Add More</button>
+                    </div>
+                    <div className="border rounded p-4 shadow mb-6">
+                    <h3 className="text-md font-semibold mb-2">Packaging Charges</h3>
+                    <p className="mb-4">Not applicable on Indian Breads, packaged items, and beverages</p>
+                    <input type="number" id="packaging_cost" name="packaging_cost" placeholder="Rs." className="border p-2 w-full mb-4" />
+                    </div>
+                  </>
                 )}
               </div>
+            ))}
+            <div className="flex justify-between">
+              {currentStep > 0 && (
+                <button type="button" onClick={backHandler} className="bg-gray-500 text-white py-2 px-4 rounded">Back</button>
+              )}
+              {currentStep < steps.length - 1 && (
+                <button type="button" onClick={nextHandler} className="bg-orange-500 text-white py-2 px-4 rounded">Proceed</button>
+              )}
+              {currentStep === steps.length - 1 && (
+                <button type="submit" className="bg-green-500 text-white py-2 px-4 rounded">Submit</button>
+              )}
+            </div>
             </form>
           </div>
         </div>
