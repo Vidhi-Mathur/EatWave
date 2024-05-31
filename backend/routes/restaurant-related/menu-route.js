14b0644a987d1c8182d83a81248b148b0fbee106
Vidhi <mathurvidhi2505@gmail.com>
2024-05-31 12:51:33 +0530

    Added authorization middleware to specific routes

--- backend/routes/restaurant-related/menu-route.js
+++ backend/routes/restaurant-related/menu-route.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const menuController = require('../../controllers/restaurant-related/menu-controller')
+const {authorizationMiddleware } = require('../../controllers/user-related/authentication-controller')
 
 //GET /eatwave/restaurant/menu/:id
 router.get('/:id', menuController.getMenuById)
+
+router.use(authorizationMiddleware)
 
 //POST /eatwave/restaurant/menu/new
 router.post('/new', menuController.createMenu)
--- backend/routes/restaurant-related/restaurant-route.js
+++ backend/routes/restaurant-related/restaurant-route.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const restaurantController = require('../../controllers/restaurant-related/restaurant-controller')
+const {authorizationMiddleware } = require('../../controllers/user-related/authentication-controller')
 
 //GET /eatwave/restaurant/:id
 router.get('/:id', restaurantController.getRestaurantById)
+
+router.use(authorizationMiddleware)
 
 //POST /eatwave/restaurant/new
 router.post('/new', restaurantController.createRestaurant)
