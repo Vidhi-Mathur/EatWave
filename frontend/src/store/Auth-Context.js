2b8e8dbd214a08dd7407fbbb113b8e17e51556a2
Vidhi <mathurvidhi2505@gmail.com>
2024-06-01 17:31:18 +0530

    Added states and functions for refreshing tokens

--- frontend/src/store/Auth-Context.js
+++ frontend/src/store/Auth-Context.js
@@ -6,35 +6,62 @@ export const AuthContext = createContext({
     signup: () => {},
     logout: () => {},
     token: null,
-    setToken: () => {} 
+    refreshToken: null,
+    setToken: () => {},
+    setRefreshToken: () => {},
+    fetchRefreshToken: () => {}
 });
 
 export const AuthCtxProvider = ({ children }) => {
     const [auth, setAuth] = useState(false);
     const [token, setToken] = useState(null); 
+    const [refreshToken, setRefreshToken] = useState(null)
 
-    const login = (newToken) => {
+    const login = (newToken, newRefreshToken) => {
         setAuth(true);
         setToken(newToken); 
+        setRefreshToken(newRefreshToken)
     };
 
-    const signup = (newToken) => {
+    const signup = (newToken, newRefreshToken) => {
         setAuth(true);
         setToken(newToken); 
+        setRefreshToken(newRefreshToken)
     };
 
     const logout = () => {
         setAuth(false);
         setToken(null); 
+        setRefreshToken(null)
     };
+
+    const fetchRefreshToken = async() => {
+        const response = await fetch('http://localhost:3000/refresh-token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ refreshToken })
+        })
+        if(!response.ok){
+            console.log('Problem with refresh token')
+            logout()
+        }
+        const result = await response.json()
+        setToken(result.accessToken)
+        return result
+    }
 
     const ctxValue = {
         isAuthenticated: auth,
         login,
         signup,
         logout,
-        token, 
-        setToken 
+        token,
+        refreshToken, 
+        setToken,
+        setRefreshToken,
+        fetchRefreshToken
     };
 
     return <AuthContext.Provider value={ctxValue}>{children}</AuthContext.Provider>;
