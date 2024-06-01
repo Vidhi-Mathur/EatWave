7f04bfda8af8f78c8fbbee2ab851834251f7cd77
Vidhi <mathurvidhi2505@gmail.com>
2024-06-01 17:36:26 +0530

    Middleware added for generating, and refreshing tokens

--- backend/controllers/user-related/authentication-controller.js
+++ backend/controllers/user-related/authentication-controller.js
@@ -1,10 +1,15 @@
+require('dotenv').config()
 const User = require('../../models/user-related/user-model')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 let blacklistedTokens = []
 
-const generateJWTToken = async(payload) => {
-    return jwt.sign(payload, process.env.SECRET_KEY)
+const generateAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.ACCESS_SECRET_KEY)
+}
+
+const generateRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.REFRESH_SECRET_KEY)
 }
 
 exports.postSignup = async(req, res, next) => {
@@ -22,10 +27,12 @@ exports.postSignup = async(req, res, next) => {
             email,
             password: hashedPassword
         })
-        await newUser.save()
         //Generating token
-        let token = await generateJWTToken({_id: newUser._id})
-        res.status(200).json({token: token, message: newUser})
+        let accessToken = generateAccessToken({ _id: newUser._id })
+        let refreshToken = generateRefreshToken({ _id: newUser._id })
+        newUser.refreshToken = refreshToken
+        await newUser.save()
+        res.status(200).json({accessToken, refreshToken, message: newUser})
     }
     catch(err) {
         next(err)
@@ -45,8 +52,12 @@ exports.postLogin = async(req, res, next) => {
         if(!validPassword){
             return res.status(401).json({message: 'Incorrect password'})
         }
-        let token = await generateJWTToken({_id: existingUser._id})
-        res.status(200).json({token: token, message: 'Logged in successfully'})
+        //Generating token
+        let accessToken = generateAccessToken({ _id: existingUser._id })
+        let refreshToken = generateRefreshToken({ _id: existingUser._id })
+        existingUser.refreshToken = refreshToken
+        await existingUser.save()
+        res.status(200).json({accessToken, refreshToken, message: 'Logged in successfully'})
     }
     catch(err){
         next(err)
@@ -60,7 +71,24 @@ exports.postLogout = async(req, res, next) => {
         //Not a token
         let token = await authorization.split(' ')[1]
         blacklistedTokens.push(token)
+        const email = jwt.decode(token)
+        await User.findByIdAndUpdate({email}, {$unset: {refreshToken: 1}})
         res.status(200).json({message: 'Logged out successfully'})
+    }
+    catch(err) {
+        next(err)
+    }
+}
+
+exports.refreshToken = async(req, res, next) => {
+    try {
+        const { refreshToken } = req.body
+        if(!refreshToken) return res.status(401).json({ message: 'Unauthorized' })
+        let decodedToken = await jwt.verify(token, process.env.REFRESH_SECRET_KEY)
+        const user = await User.findById(decodedToken._id)
+        if(!user || user.refreshToken !== refreshToken) return res.status(401).json({ message: 'Unauthorized' })
+        const newAccessToken = generateAccessToken({_id: user._id})
+        res.status(200).json({ accessToken: newAccessToken })
     }
     catch(err) {
         next(err)
@@ -76,7 +104,7 @@ exports.authorizationMiddleware = async(req, res, next) => {
         let token = await authorization.split(' ')[1]
         if(!token || blacklistedTokens.includes(token)) return res.status(401).json({message: 'Unauthorized'})
         //Verify
-        let decodedToken = await jwt.verify(token, process.env.SECRET_KEY)
+        let decodedToken = await jwt.verify(token, process.env.ACCESS_SECRET_KEY)
         req.user = decodedToken
         next()
     }
