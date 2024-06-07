be3f3e4bd37b2b995593d99e57fed239d3474dbe
Vidhi <mathurvidhi2505@gmail.com>
2024-06-07 20:51:33 +0530

    Added Cloudinary for image management

---
+++ backend/util/cloudinary.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const cloudinary = require('cloudinary').v2
+const fs = require("fs")
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_SECRET_KEY
+});
+
+exports.fileUploadCloudinary = async(localStoragePath) => {
+    try {
+        if(!localStoragePath) return null;
+        const response = await cloudinary.uploader.upload(localStoragePath, {
+            resource_type: "auto"
+        });
+        console.log(response.url)
+        return response
+    }
+    catch(err) {
+        //Remove temporary saved file as upload failed
+        fs.unlinkSync(localStoragePath)
+        return null
+    }
+}
