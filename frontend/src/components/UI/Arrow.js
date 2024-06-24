e31f5ead90681ffd10b1208c9ea516c89a19cd01
Vidhi <mathurvidhi2505@gmail.com>
2024-06-24 19:22:53 +0530

    Custom arrow component instead of both next and previous arrows

--- frontend/src/components/UI/Arrow.js
+++ frontend/src/components/UI/Arrow.js
@@ -1,19 +1,14 @@
 import { ArrowBackIos, ArrowForwardIos } from '@mui/icons-material';
 
-export const PreviousArrow = (props) => {
-    const { style, onClick } = props
+export const Arrow = ({ style, onClick, direction }) => {
     return (
-        <div className="slick-arrow" style={{...style, display: 'block', cursor: 'pointer', zIndex: 1}} onClick={onClick}> 
-        <ArrowBackIos style={{ color: 'black', fontSize: '30px'}}/>
-        </div>
-    )
-}
-
-export const NextArrow = (props) => {
-    const { style, onClick } = props
-    return (
-        <div className="slick-arrow" style={{...style, display: 'block', cursor: 'pointer', zIndex: 1}} onClick={onClick}> 
-        <ArrowForwardIos style={{ color: 'black', fontSize: '30px'}}/>
+        <div className={`slick-arrow block absolute top-1/2 transform -translate-y-1/2 z-10 cursor-pointer ${direction === 'left'? 'left-2': 'right-2'}`} style={style} onClick={onClick}> 
+        {direction === 'left'? (
+            <ArrowBackIos style={{ color: 'black', fontSize: '30px'}}/>
+        ): 
+        (
+            <ArrowForwardIos style={{ color: 'black', fontSize: '30px'}}/>
+        )}
         </div>
     )
 }
