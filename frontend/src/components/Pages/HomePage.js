00e69816281a0003c72a4cc448e78e1350bd0149
Vidhi <mathurvidhi2505@gmail.com>
2024-06-26 19:52:59 +0530

    Updates state and adjusted styling

--- frontend/src/components/Pages/HomePage.js
+++ frontend/src/components/Pages/HomePage.js
@@ -1,15 +1,10 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense } from "react";
 import Layout from "../UI/Layout";
 import TuneIcon from '@mui/icons-material/Tune';
-import SortIcon from '@mui/icons-material/Sort';
-import { Sort } from "../UI/Sort";
+import { SortMenu } from "../UI/SortMenu";
 let TopPicks = React.lazy(() => import("../restaurant-related/TopPicks"))
 
 const HomePage = () => {
-  const [showSort, setShowSort] = useState(false)
-  const toggleSort = () => {
-    setShowSort(prevState => !prevState)
-  }
   return (
     <Layout>
       <Suspense fallback="Loading">
@@ -18,8 +13,7 @@ const HomePage = () => {
       <div className="inline-flex items-center space-x-4">
       <h1>Choose what you want </h1>
       <button className="mr-2 mb-2 px-2 py-1 border rounded bg-white shadow-md">Filter <TuneIcon /></button>
-      <button className="mr-2 mb-2 px-2 py-1 border rounded bg-white shadow-md" onClick={toggleSort}>Sort By <SortIcon /></button>
-      {showSort && <Sort />}
+      {<SortMenu />}
       </div>
     </Layout>
   );
