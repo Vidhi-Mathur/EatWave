1e202e70ec28e56f72c129a71322d718f86a40bd
Vidhi <mathurvidhi2505@gmail.com>
2024-07-02 13:22:18 +0530

    Manage state to display restaurants after filtering and sorting

--- frontend/src/components/Pages/HomePage.js
+++ frontend/src/components/Pages/HomePage.js
@@ -1,10 +1,13 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import Layout from "../UI/Layout";
 import { SortMenu } from "../UI/SortMenu";
 import { FilterDialog } from "../UI/FilterDialog";
+import { SortedRestaurants } from "../restaurant-related/SortedRestaurants";
 let TopPicks = React.lazy(() => import("../restaurant-related/TopPicks"))
 
 const HomePage = () => {
+const [restaurants, setRestaurants] = useState([])
+
   return (
     <Layout>
       <Suspense fallback="Loading">
@@ -13,10 +16,11 @@ const HomePage = () => {
       <div>
         <div className="inline-flex items-center space-x-4">
           <h1 className="ml-10 text-3xl font-bold text-white bg-gray-800 bg-opacity-75 px-4 py-2 rounded-lg shadow-lg">Choose what you want </h1>
-          <FilterDialog/>
+          <FilterDialog setRestaurants = {setRestaurants} />
         </div>
-        <SortMenu />
+        <SortMenu setRestaurants = {setRestaurants}/>
       </div>
+      <SortedRestaurants restaurants = {restaurants} />
     </Layout>
   );
 };
