4eab75ffdc434b4cddcf86d8fa32278095c454aa
Vidhi <mathurvidhi2505@gmail.com>
2024-07-03 12:54:13 +0530

    Added logic for sorting through cost, and filtering on ratings, food preference and cost for two

--- backend/controllers/restaurant-related/restaurant-controller.js
+++ backend/controllers/restaurant-related/restaurant-controller.js
@@ -63,7 +63,6 @@ exports.sortRestaurantsByDefault = async (req, res, next) => {
 
 exports.sortRestaurantsByRatings = async(req, res, next) => {
     try {
-        //Fetch all restaurants
         const restaurants = await Restaurant.find({}).sort({averageRating: -1})
         res.status(200).json({ restaurants })
     }
@@ -72,10 +71,19 @@ exports.sortRestaurantsByRatings = async(req, res, next) => {
     }
 }
 
-exports.filterRestaurantsByPreference = async(req, res, next) => {
+exports.sortRestaurantsByLowToHighCost = async(req, res, next) => {
     try {
-        const { preference } = req.body
-        const restaurants = await Restaurant.find({ foodType: preference })
+        const restaurants = await Restaurant.find({}).sort({costForTwo: 1})
+        res.status(200).json({ restaurants })
+    }
+    catch(err) {
+        next(err)
+    }
+}
+
+exports.sortRestaurantsByHighToLowCost = async(req, res, next) => {
+    try {
+        const restaurants = await Restaurant.find({}).sort({costForTwo: -1})
         res.status(200).json({ restaurants })
     }
     catch(err) {
@@ -90,6 +98,40 @@ exports.filterRestaurantsByCuisines = async(req, res, next) => {
         res.status(200).json({ restaurants })
     }
     catch(err){
+        next(err)
+    }
+}
+
+exports.filterRestaurantsByPreference = async(req, res, next) => {
+    try {
+        const { preference } = req.body
+        let restaurants
+        if(preference === "Veg"){
+            restaurants = await Restaurant.find({ foodType: "Veg"})
+        }
+        else {
+            restaurants = await Restaurant.find({ foodType: "Both"})
+        }
+        res.status(200).json({ restaurants })
+    }
+    catch(err) {
+        next(err)
+    }
+}
+
+exports.filterRestaurantsByRatings = async(req, res, next) => {
+    try {
+        const { ratings } = req.body
+        let range = []
+        for(let rating in ratings){
+            range.push({ averageRating: { $gte: rating }})
+        }
+        const query = range.length ? { $or: range } : {};
+        const restaurants = await Restaurant.find(query)
+        res.status(200).json({ restaurants })
+    }
+    catch(err){
+        console.log(err)
         next(err)
     }
 }
