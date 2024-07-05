348755e987b9b515ae42d2404c16a1a55557aa75
Vidhi <mathurvidhi2505@gmail.com>
2024-07-05 12:38:30 +0530

    Sending request to backend to update user's cart

---
+++ frontend/src/services/CartService.js
@@ -0,0 +1,31 @@
+export const getCartAPI = async (token) => {
+    const response = await fetch('http://localhost:3000/cart', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    if (!response.ok) {
+        throw new Error("Failed to get cart");
+    }
+    const result = await response.json();
+    console.log(result)
+    return result;
+};
+
+export const updateCartAPI = async (token, { updates, restaurant }) => {
+    const response = await fetch('http://localhost:3000/cart/update', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ updates, restaurant })
+    });
+    if (!response.ok) {
+        throw new Error("Failed to update cart");
+    }
+    const result = await response.json();
+    return result;
+};
