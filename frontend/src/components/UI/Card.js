f8cfc2f94753cc10a3e71378704d789d651a1cdd
Vidhi <mathurvidhi2505@gmail.com>
2024-07-06 12:56:51 +0530

    Adjusted styling

--- frontend/src/components/UI/Card.js
+++ frontend/src/components/UI/Card.js
@@ -1,7 +1,7 @@
-const Card = ({children}) => {
+const Card = ({ children, className }) => {
     return (
-        <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md mt-8">{children}</div>
-    )
+        <div className={`max-w-md mx-auto bg-white rounded-lg shadow-md mt-8 ${className}`}>{children}</div>
+    );
 }
 
-export default Card
+export default Card;
