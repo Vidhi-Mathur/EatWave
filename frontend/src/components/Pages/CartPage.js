5230f6dbf0dea445b0ac0abf29d024fa5bca22c8
Vidhi <mathurvidhi2505@gmail.com>
2024-07-06 19:45:47 +0530

    Sending background url as prop and modified styling for empty cart

--- frontend/src/components/Pages/CartPage.js
+++ frontend/src/components/Pages/CartPage.js
@@ -4,7 +4,9 @@ import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
 import Layout from "../UI/Layout";
 import Content from "../UI/Content";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import EmptyPlate from '../../assets/pexels-enginakyurt-1907642.jpg'
+import Card from "../UI/Card";
 
 export const CartPage = () => {
   const { items, addToCart, removeFromCart } = useContext(CartContext);
@@ -12,9 +14,21 @@ export const CartPage = () => {
     (sum, item) => sum + item.price * item.quantity,
     0
   );
+
+  const emptyCart = items.length === 0 ? EmptyPlate : null;
+
   return (
-    <Layout>
-      <Content>
+      <Layout customisedImageUrl={emptyCart}>   
+      {emptyCart? (
+        <Card className="p-6 bg-opacity-75">
+          <div className="flex flex-col items-center justify-center h-full">
+              <h1 className="text-4xl font-bold text-center text-gray-900 mb-4">Your Cart is Empty</h1>
+              <p className="text-lg text-center text-gray-700 mb-4">You can go to home page to view more restaurants</p>
+              <Link to="/" className="bg-orange-400 hover:bg-orange-600 text-white font-semibold py-2 px-6 rounded-lg transition-colors duration-300 ease-in-out">SEE RESTAURANTS NEAR YOU</Link>
+          </div>
+        </Card>
+        ): (
+        <Content>
         <h2 className="text-2xl font-bold mb-4 pt-2 pl-2">Shopping Cart</h2>
         {items.map((item) => {
           return (
@@ -41,17 +55,17 @@ export const CartPage = () => {
           );
         })}
         <div className="mt-8 pb-2 pl-2 flex justify-between">
-        {totalPrice === 0 && <h3 className="text-lg font-semibold">Nothing added</h3>}
         {totalPrice > 0 && (
           <>
-  <h3 className="text-lg font-semibold">Total: &#8377;{totalPrice.toFixed(2)}</h3>
-  <button className="py-2 px-6 mr-2 mb-4 bg-orange-400 text-white rounded-md hover:bg-orange-500 focus:outline-none focus:bg-orange-500">
-    <NavLink to="/order">Order</NavLink>
-  </button>
-  </>
-  )}
-</div>
+          <h3 className="text-lg font-semibold">Total: &#8377;{totalPrice.toFixed(2)}</h3>
+          <button className="py-2 px-6 mr-2 mb-4 bg-orange-400 text-white rounded-md hover:bg-orange-500 focus:outline-none focus:bg-orange-500">
+            <NavLink to="/order">Order</NavLink>
+          </button>
+          </>
+        )}
+      </div>
       </Content>
+      )}
     </Layout>
   );
 };
