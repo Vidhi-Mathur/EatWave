b6f3a1cbcb3a15621b8ebcaf7b3012f7a3e115dc
Vidhi <mathurvidhi2505@gmail.com>
2024-07-06 12:58:25 +0530

    Adjusted styling, displays cuisines, address and cost for two

--- frontend/src/components/restaurant-related/TopPicks.js
+++ frontend/src/components/restaurant-related/TopPicks.js
@@ -61,17 +61,31 @@ const TopPicks = () => {
                 <Slider {...settings} className='mx-4'>
                     {topRestaurants.map((restaurant, idx) => (
                        <div key={idx} className='p-2'>
-                            <Card className="bg-white rounded-lg shadow-md p-4">
-                            <Link to={`/restaurant/${restaurant._id}`}>
-                            <div className="rounded-lg overflow-hidden border border-gray-300 h-48" >
-                                <img src={restaurant.imageUrls[0]} alt={restaurant.restaurantName} className="w-full h-full object-cover" />
-                            </div>
-                            <div className="flex justify-between mb-4 mt-4">
-                                <h2 className="text-xl font-semibold">{restaurant.restaurantName}</h2>
-                                <ReviewStar rating={restaurant.averageRating} />
-                            </div>
-                            </Link>
-                        </Card>
+                        <Card className="p-4">
+                                <Link to={`/restaurant/${restaurant._id}`} className="flex flex-col h-full">
+                                    <div className="relative h-48">
+                                        <img src={restaurant.imageUrls[0]} alt={restaurant.restaurantName} className="w-full h-full object-cover" />
+                                        {/* ToDo - Manage offers later */}
+                                        {/* <div className="absolute bottom-2 left-2 bg-orange-500 text-white px-2 py-1 rounded-sm text-sm">Flat 20% off</div> */}
+                                    </div>
+                                    <div className="p-3 flex flex-col flex-grow">
+                                        <div className="flex justify-between items-start mb-2">
+                                            <div className="flex-grow pr-2">
+                                                <h2 className="text-lg font-semibold truncate">{restaurant.restaurantName}</h2>
+                                                <p className="text-md text-gray-600 truncate">
+                                                    {restaurant.cuisine.slice(0, 2).join(', ')}
+                                                    {restaurant.cuisine.length > 2 ? '...' : ''}
+                                                </p>
+                                            </div>
+                                            <div className="flex flex-col items-end">
+                                                <ReviewStar rating={restaurant.averageRating} />
+                                                <p className="text-md text-gray-600 mt-1">&#8377;{restaurant.costForTwo} for two</p>
+                                            </div>
+                                        </div>
+                                        <p className="text-md text-gray-500 truncate mt-auto">{restaurant.address.street}, {restaurant.address.city}</p>
+                                    </div>
+                                </Link>
+                            </Card>
                        </div>
                     ))}
                 </Slider>
