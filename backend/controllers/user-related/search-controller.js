9df9ac759e2591d7be5315609d31a56e2ad66f67
Vidhi <mathurvidhi2505@gmail.com>
2024-07-09 23:08:41 +0530

    Includes averageRating, costForTwo and cuisine for restaurant and price for dish

--- backend/controllers/user-related/search-controller.js
+++ backend/controllers/user-related/search-controller.js
@@ -5,8 +5,8 @@ const performSearch = async({query, limit = null}) => {
         if (typeof query !== 'string' || query.trim() === '') {
             throw new Error('Invalid query');
         }
-        // Fetch restaurants matching the query
-        let restaurantQuery = Restaurant.find({ restaurantName: { $regex: query, $options: 'i' }}).select('restaurantName imageUrls').lean()
+        // Fetch restaurants matching the query to access restaurantName, imageUrls and averageRating
+        let restaurantQuery = Restaurant.find({ restaurantName: { $regex: query, $options: 'i' }}).select('restaurantName imageUrls averageRating costForTwo cuisine').lean()
         if(limit){
             restaurantQuery = restaurantQuery.limit(limit)
         }
@@ -14,18 +14,27 @@ const performSearch = async({query, limit = null}) => {
         let restaurantSuggestions = restaurants.map(restaurant => ({
             _id: restaurant._id,
             restaurantName: restaurant.restaurantName,
-            imageUrl: restaurant.imageUrls[0]
+            imageUrl: restaurant.imageUrls[0],
+            averageRating: restaurant.averageRating,
+            costForTwo: restaurant.costForTwo,
+            cuisine: restaurant.cuisine
         }));
         // Fetch menu items matching query
-        let menuQuery = Menu.find({'items.name': { $regex: query, $options: 'i' }}).select('items.name items._id').lean()
+        let menuQuery = Menu.find({'items.name': { $regex: query, $options: 'i' }}).select('items.name items._id items.price').populate('restaurant', 'restaurantName averageRating').lean()
         if(limit){
             menuQuery = menuQuery.limit(limit)
         }
         let menuSuggestions = await menuQuery
         //Map items with name and id
-        let dishSuggestions = menuSuggestions.flatMap(menu => menu.items)
+        let dishSuggestions = menuSuggestions.flatMap(menu => menu.items
             .filter(item => item.name.toLowerCase().includes(query.toLowerCase()))
-            .map(item => ({ _id: item._id, name: item.name }))
+            .map(item => ({ 
+                _id: item._id, 
+                name: item.name, 
+                price: item.price, 
+                restaurantName: menu.restaurant.restaurantName, 
+                restaurantRating: menu.restaurant.averageRating
+        })))
         return {
             restaurants: restaurantSuggestions,
             dishes: limit? dishSuggestions.slice(0, limit): dishSuggestions
