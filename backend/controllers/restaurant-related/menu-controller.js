bff4c7e6203d20317cb03a1e92d1dcb92ef77d26
Vidhi <mathurvidhi2505@gmail.com>
2024-07-13 19:13:43 +0530

    Replaced itemNotFound[] with boolean variable, modified reponse send

--- backend/controllers/restaurant-related/menu-controller.js
+++ backend/controllers/restaurant-related/menu-controller.js
@@ -3,7 +3,7 @@ const Menu = require('../../models/restaurant-related/menu-model')
 exports.createMenu = async(req, res, next) => {
     try {
         const { items } = req.body
-        if(items.length == 0) return res.status(400).json({ message: 'No items added to the menu' })
+        if(items.length === 0) return res.status(400).json({ message: 'No items added to the menu' })
         const menu = await Menu.create({
             items: items,
             restaurant: null
@@ -17,8 +17,8 @@ exports.createMenu = async(req, res, next) => {
 
 exports.getMenuById = async(req, res, next) => {
     try {
-        const { id } = req.params
-        const menu = await Menu.findById(id)
+        const { menuId } = req.params
+        const menu = await Menu.findById(menuId)
         if(!menu) return res.status(404).json({ message: 'No associated menu found' })
         res.status(200).json({ menu: menu.items })
     }
@@ -33,19 +33,19 @@ exports.updateMenu = async (req, res, next) => {
         const menu = await Menu.findById(menuId);
         if (!menu) return res.status(404).json({ message: 'No associated menu found' });
         const { items } = req.body;
-        const notFoundItems = [];
+        let itemNotFound = false
         items.forEach(updatedItem => {
             const itemId = updatedItem._id;
             const item = menu.items.id(itemId);
             if (!item) {
-                notFoundItems.push(itemId);
+                itemNotFound = true
             } 
             else {
                 Object.assign(item, updatedItem);
             }
         });
-        if (notFoundItems.length > 0) {
-            return res.status(404).json({ message: 'Some items not found', notFoundItems });
+        if (itemNotFound) {
+            return res.status(404).json({ message: 'Item missing in database' });
         }
         await menu.save();
         res.status(200).json({ menu });
