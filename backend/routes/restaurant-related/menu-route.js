2adbfaee527bdfe48f5662f953c5607c2761348d
Vidhi <mathurvidhi2505@gmail.com>
2024-07-14 22:57:26 +0530

    Added validation middlewares to menu routes

--- backend/routes/restaurant-related/menu-route.js
+++ backend/routes/restaurant-related/menu-route.js
@@ -2,16 +2,17 @@ const express = require('express')
 const router = express.Router()
 const menuController = require('../../controllers/restaurant-related/menu-controller')
 const {authorizationMiddleware } = require('../../controllers/user-related/authentication-controller')
+const { createMenuValidation, getMenuByIdValidation, updateMenuValidation } = require('../../validators/restaurant-related/menu-validator')
 
 //GET /eatwave/restaurant/menu/:menuId
-router.get('/:menuId', menuController.getMenuById)
+router.get('/:menuId',getMenuByIdValidation, menuController.getMenuById)
 
 router.use(authorizationMiddleware)
 
 //POST /eatwave/restaurant/menu/new
-router.post('/new', menuController.createMenu)
+router.post('/new', createMenuValidation, menuController.createMenu)
 
 //PATCH  /eatwave/restaurant/menu/:menuId
-router.patch('/:menuId', menuController.updateMenu)
+router.patch('/:menuId', updateMenuValidation, menuController.updateMenu)
 
 module.exports = router
