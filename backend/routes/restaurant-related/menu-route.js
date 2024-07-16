50b94a7963c16c81a875a83513b22159553f8a0a
Vidhi <mathurvidhi2505@gmail.com>
2024-07-16 23:26:22 +0530

    Destructed middlewares imported

--- backend/routes/restaurant-related/menu-route.js
+++ backend/routes/restaurant-related/menu-route.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const router = express.Router()
-const menuController = require('../../controllers/restaurant-related/menu-controller')
+const { getMenuById, createMenu, updateMenu} = require('../../controllers/restaurant-related/menu-controller')
 const {authorizationMiddleware } = require('../../controllers/user-related/authentication-controller')
 const { createMenuValidation, getMenuByIdValidation, updateMenuValidation } = require('../../validators/restaurant-related/menu-validator')
 
 //GET /eatwave/restaurant/menu/:menuId
-router.get('/:menuId',getMenuByIdValidation, menuController.getMenuById)
+router.get('/:menuId',getMenuByIdValidation, getMenuById)
 
 router.use(authorizationMiddleware)
 
 //POST /eatwave/restaurant/menu/new
-router.post('/new', createMenuValidation, menuController.createMenu)
+router.post('/new', createMenuValidation, createMenu)
 
 //PATCH  /eatwave/restaurant/menu/:menuId
-router.patch('/:menuId', updateMenuValidation, menuController.updateMenu)
+router.patch('/:menuId', updateMenuValidation, updateMenu)
 
 module.exports = router
