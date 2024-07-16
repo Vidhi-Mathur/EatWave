add37fa9be2e7466a7c52fcbd511fb2e3d146b9b
Vidhi <mathurvidhi2505@gmail.com>
2024-07-16 23:25:50 +0530

    Destructed middlewares imported

--- backend/routes/restaurant-related/restaurant-route.js
+++ backend/routes/restaurant-related/restaurant-route.js
@@ -1,45 +1,45 @@
 const express = require('express')
 const router = express.Router()
-const restaurantController = require('../../controllers/restaurant-related/restaurant-controller')
+const { getRestaurantById, sortRestaurantsByDefault, sortRestaurantsByRatings, sortRestaurantsByLowToHighCost, sortRestaurantsByHighToLowCost, filterRestaurantsByCuisines, filterRestaurantsByRatings, filterRestaurantsByPreference, filterRestaurantsByCostForTwo, createRestaurant, updateRestaurant, deleteRestaurant } = require('../../controllers/restaurant-related/restaurant-controller')
 const {authorizationMiddleware } = require('../../controllers/user-related/authentication-controller')
 const { restaurantIdValidation, createRestaurantValidation, updateRestaurantValidation, filterRestaurantsByCuisinesValidation, filterRestaurantsByCostForTwoValidation, filterRestaurantsByPreferenceValidation, filterRestaurantsByRatingsValidation } = require("../../validators/restaurant-related/restaurant-validator")
 
 //GET /eatwave/restaurant/:restaurantId
-router.get('/:restaurantId', restaurantIdValidation, restaurantController.getRestaurantById)
+router.get('/:restaurantId', restaurantIdValidation, getRestaurantById)
 
 //GET /eatwave/restaurant/sort/default
-router.get('/sort/default', restaurantController.sortRestaurantsByDefault)
+router.get('/sort/default', sortRestaurantsByDefault)
 
 //GET /eatwave/restaurant/sort/ratings
-router.get('/sort/ratings', restaurantController.sortRestaurantsByRatings)
+router.get('/sort/ratings', sortRestaurantsByRatings)
 
 //GET /eatwave/restaurant/sort/cost-low-to-high
-router.get('/sort/cost-low-to-high', restaurantController.sortRestaurantsByLowToHighCost)
+router.get('/sort/cost-low-to-high', sortRestaurantsByLowToHighCost)
 
 //GET /eatwave/restaurant/sort/cost-high-to-low
-router.get('/sort/cost-high-to-low', restaurantController.sortRestaurantsByHighToLowCost)
+router.get('/sort/cost-high-to-low', sortRestaurantsByHighToLowCost)
 
 //POST /eatwave/restaurant/filter/cuisines
-router.post('/filter/cuisines', filterRestaurantsByCuisinesValidation, restaurantController.filterRestaurantsByCuisines)
+router.post('/filter/cuisines', filterRestaurantsByCuisinesValidation, filterRestaurantsByCuisines)
 
 //POST /eatwave/restaurant/filter/ratings
-router.post('/filter/ratings', filterRestaurantsByRatingsValidation, restaurantController.filterRestaurantsByRatings)
+router.post('/filter/ratings', filterRestaurantsByRatingsValidation, filterRestaurantsByRatings)
 
 //POST /eatwave/restaurant/filter/preference
-router.post('/filter/preference', filterRestaurantsByPreferenceValidation, restaurantController.filterRestaurantsByPreference)
+router.post('/filter/preference', filterRestaurantsByPreferenceValidation, filterRestaurantsByPreference)
 
 //POST /eatwave/restaurant/filter/cost-for-two
-router.post('/filter/cost-for-two', filterRestaurantsByCostForTwoValidation, restaurantController.filterRestaurantsByCostForTwo)
+router.post('/filter/cost-for-two', filterRestaurantsByCostForTwoValidation, filterRestaurantsByCostForTwo)
 
 router.use(authorizationMiddleware)
 
 //POST /eatwave/restaurant/new
-router.post('/new', createRestaurantValidation, restaurantController.createRestaurant)
+router.post('/new', createRestaurantValidation, createRestaurant)
 
 //PATCH /eatwave/restaurant/:restaurantId
-router.patch('/:restaurantId', updateRestaurantValidation, restaurantController.updateRestaurant)
+router.patch('/:restaurantId', updateRestaurantValidation, updateRestaurant)
 
 //DELETE /eatwave/restaurant/:restaurantId
-router.delete('/:restaurantId', restaurantIdValidation, restaurantController.deleteRestaurant)
+router.delete('/:restaurantId', restaurantIdValidation, deleteRestaurant)
 
 module.exports = router
