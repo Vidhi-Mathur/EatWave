47011f060782558c7dd6b3a159b28f601c001fd7
Vidhi <mathurvidhi2505@gmail.com>
2024-07-16 23:19:33 +0530

    Added validation middlewares related to order and reviews

---
+++ backend/validators/shared/order-validator.js
@@ -0,0 +1,39 @@
+const { body, param } = require("express-validator")
+const { handleValidationErrors } = require("../common/handleValidationErrors")
+
+exports.initiatePaymentValidation = [
+    body("restaurant").notEmpty().withMessage("RestaurantId is required").bail().isMongoId().withMessage("Invalid RestaurantId"),
+    body("items").notEmpty("Items can't be empty").bail().isArray().withMessage("Items must be an array"),
+    body("items.*.item").notEmpty().withMessage("ItemId is missing").bail().isMongoId().withMessage("Invalid RestaurantId"),
+    body('items.*.quantity').isInt({ min: 1 }).withMessage('Quantity must be a positive integer'),
+    body('totalAmount').isFloat({ min: 0 }).withMessage('Total amount must be a positive number'),
+    handleValidationErrors
+]
+
+exports.placeOrderValidation = [
+    body("restaurant").notEmpty().withMessage("RestaurantId is required").bail().isMongoId().withMessage("Invalid RestaurantId"),
+    body("items").notEmpty("Items can't be empty").bail().isArray().withMessage("Items must be an array"),
+    body("items.*.item").notEmpty().withMessage("ItemId is missing").bail().isMongoId().withMessage("Invalid RestaurantId"),
+    body('items.*.quantity').isInt({ min: 1 }).withMessage('Quantity must be a positive integer'),
+    body('totalAmount').isFloat({ min: 0 }).withMessage('Total amount must be a positive number'),
+    body("address").isObject().withMessage("Address must be and object"),
+    body("address.street").notEmpty().withMessage("Street is required").bail().isString().withMessage("Invalid Street Format"),
+    body("address.city").notEmpty().withMessage("City is required").bail().isString().withMessage("Invalid City Format"),
+    body("address.state").notEmpty().withMessage("State is required").bail().isString().withMessage("Invalid state Format"),
+    body("address.postalCode").notEmpty().withMessage("Postal Code is required").bail().isString().withMessage("Invalid Postal Code format"),
+    body('payment_id').notEmpty().withMessage('PaymentId is required'),
+    body('order_id').notEmpty().withMessage('OrderId is required'),
+    body('signature').notEmpty().withMessage('Signature is required'),
+    handleValidationErrors
+]
+
+exports.getOrderByOrderIdValidation = [
+    param('orderId').notEmpty().withMessage("OrderId is required").bail().isMongoId().withMessage('Invalid orderID'),
+    handleValidationErrors
+]
+
+exports.getRestaurantOrderHistoryValidation = [
+    param('restaurantId').notEmpty().withMessage("RestaurantId is required").bail().isMongoId().withMessage('Invalid restaurantId'),
+    handleValidationErrors
+]
+
---
+++ backend/validators/shared/review-validator.js
@@ -0,0 +1,32 @@
+const { body, param } = require("express-validator")
+const { handleValidationErrors } = require("../common/handleValidationErrors")
+
+exports.createReviewValidation = [
+    param("orderId").isMongoId().withMessage("Valid OrderId is required"), 
+    body("restaurant").isMongoId().withMessage("Valid RestaurantId is required"),
+    body("reviewer").isMongoId().withMessage("Valid ReviewerId is required"),
+    body("rating").isFloat({ min: 0, max: 5 }).withMessage("Invalid rating, should be a number between 0 & 5"),
+    body("comments").optional().isString(),
+    body("imageUrl").optional().isURL(), 
+    handleValidationErrors
+]
+
+exports.updateReviewValidation = [
+    param("reviewId").isMongoId().withMessage("Valid ReviewId is required"), 
+    body("restaurant").isMongoId().withMessage("Valid RestaurantId is required"),
+    body("reviewer").isMongoId().withMessage("Valid ReviewerId is required"),
+    body("rating").isFloat({ min: 0, max: 5 }).withMessage("Invalid rating, should be a number between 0 & 5"),
+    body("comments").optional().isString(),
+    body("imageUrl").optional().isURL(),
+    handleValidationErrors
+]
+
+exports.getReviewByOrderIdValidation = [
+    param("orderId").isMongoId().withMessage("Valid OrderId is required"),
+    handleValidationErrors
+]
+
+exports.getReviewsByRestaurantIdValidation = [
+    param("restaurantId").isMongoId().withMessage("Valid restaurantId is required"),
+    handleValidationErrors
+]
