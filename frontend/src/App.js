19cde7639971755601324389e47e5aaff7d12351
Vidhi <mathurvidhi2505@gmail.com>
2024-07-17 13:35:05 +0530

    Add ProtectedRoute component to unable access to unauthorized users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/Pages/HomePage';
 import AuthFormPage from './components/Pages/AuthPage.js';
 import TermsAndConditions from './components/Pages/TermsAndConditionsPage';
 import RestaurantDetailPage from './components/Pages/RestaurantDetailPage.js';
-import { AuthContext } from './store/Auth-Context.js';
 import { CartPage } from './components/Pages/CartPage.js';
 import { OrderPage } from './components/Pages/OrderPage.js';
 import { ErrorPage } from './components/Pages/ErrorPage.js';
@@ -14,9 +13,9 @@ import { AccountPage } from './components/Pages/AccountPage.js';
 import { PastOrders } from './components/user-related/PastOrders.js';
 import { AddRestaurantPage } from './components/Pages/AddRestaurantPage.js';
 import { SearchPage } from './components/Pages/SearchPage.js';
+import { ProtectedRoutes } from './components/user-related/ProtectedRoutes.js';
 
 function App() {
-  const { isAuthenticated } = useContext(AuthContext);
   return (
     <div className="App">
       <Router>
@@ -26,10 +25,8 @@ function App() {
               <Route path="/login" element={<AuthFormPage signup={false} />} />
               <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
               <Route path="/restaurant/:restaurantId" element={<RestaurantDetailPage />} />
-              <Route path='/restaurant/add-restaurant' element={<AddRestaurantPage />} />
               <Route path ="/search" element={<SearchPage />}/>
-              {isAuthenticated && (
-                <>
+              <Route element={<ProtectedRoutes />}>
                   <Route path='/cart' element={<CartPage />} />
                   <Route path='/order' element={<OrderPage />} />
                   <Route path='/cancel' element={<Cancel />} />
@@ -38,8 +35,8 @@ function App() {
                     <Route path="orders" element={<PastOrders />} />
                     {/* <Route path="/favorites" element={} /> */}
                   </Route>
-                </>
-              )}
+                  <Route path='/restaurant/add-restaurant' element={<AddRestaurantPage />} />
+              </Route>
               <Route path="*" element={<ErrorPage />}/>
             </Routes>
       </Router>
