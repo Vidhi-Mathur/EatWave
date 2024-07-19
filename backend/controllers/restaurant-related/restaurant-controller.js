6da2a24ab69a278c525d7f6e48a7ba2e8466cb66
Vidhi <mathurvidhi2505@gmail.com>
2024-07-19 18:51:11 +0530

    Implement combined filtering logic for sort, cuisinies, preference, ratings and cost

--- backend/controllers/restaurant-related/restaurant-controller.js
+++ backend/controllers/restaurant-related/restaurant-controller.js
@@ -50,114 +50,62 @@ exports.getRestaurantById = async(req, res, next) => {
     }
 }
 
-exports.sortRestaurantsByDefault = async (req, res, next) => {
+exports.filterRestaurants = async (req, res, next) => {
     try {
-        //Fetch all restaurants
-        const restaurants = await Restaurant.find({})
-        res.status(200).json({ restaurants })
-    }
-    catch(err) {
-        next(err)
+        const { sort, cuisines, preference, ratings, costForTwo } = req.body;
+        let query = {};
+        if(cuisines && cuisines.length > 0){
+            query.cuisine = { $in: cuisines };
+        }
+        if(preference){
+            if(preference === "Veg"){
+                query.foodType = "Veg";
+            } 
+            else{
+                query.foodType = query.foodType = "Both";
+            } 
+        }
+        if(ratings && ratings.length > 0){
+            query.averageRating = { $gte: Math.min(...ratings) };
+        }
+        if(costForTwo && costForTwo.length > 0){
+            let costQueries = [];
+            if(costForTwo.includes("Less than 300")){
+                costQueries.push({ costForTwo: { $lt: 300 } });
+            }
+            if(costForTwo.includes("Between 300-600")){
+                costQueries.push({ costForTwo: { $gte: 300, $lte: 600 } });
+            }
+            if(costForTwo.includes("Greater than 600")){
+                costQueries.push({ costForTwo: { $gt: 600 } });
+            }
+            if(costQueries.length > 0){
+                query.$or = costQueries;
+            }
+        }
+        let sortQuery = {};
+        if(sort){
+            switch (sort) {
+                case "Ratings":
+                    sortQuery.averageRating = -1;
+                    break;
+                case "Cost: Low to High":
+                    sortQuery.costForTwo = 1;
+                    break;
+                case "Cost: High to Low":
+                    sortQuery.costForTwo = -1;
+                    break;
+                default: 
+                    break;
+            }
+        }
+        const restaurants = await Restaurant.find(query).sort(sortQuery);
+        res.status(200).json({ restaurants });
+    } 
+    catch(err){
+        next(err);
     }
 };
-
-exports.sortRestaurantsByRatings = async(req, res, next) => {
-    try {
-        const restaurants = await Restaurant.find({}).sort({averageRating: -1})
-        res.status(200).json({ restaurants })
-    }
-    catch(err) {
-        next(err)
-    }
-}
-
-exports.sortRestaurantsByLowToHighCost = async(req, res, next) => {
-    try {
-        const restaurants = await Restaurant.find({}).sort({costForTwo: 1})
-        res.status(200).json({ restaurants })
-    }
-    catch(err) {
-        next(err)
-    }
-}
-
-exports.sortRestaurantsByHighToLowCost = async(req, res, next) => {
-    try {
-        const restaurants = await Restaurant.find({}).sort({costForTwo: -1})
-        res.status(200).json({ restaurants })
-    }
-    catch(err) {
-        next(err)
-    }
-}
-
-exports.filterRestaurantsByCuisines = async(req, res, next) => {
-    try {
-        const { cuisines } = req.body
-        const restaurants = await Restaurant.find({ cuisine: { $in: cuisines } });
-        res.status(200).json({ restaurants })
-    }
-    catch(err){
-        next(err)
-    }
-}
-
-exports.filterRestaurantsByPreference = async(req, res, next) => {
-    try {
-        const { preference } = req.body
-        let restaurants
-        if(preference === "Veg"){
-            restaurants = await Restaurant.find({ foodType: "Veg"})
-        }
-        else {
-            restaurants = await Restaurant.find({ foodType: "Both"})
-        }
-        res.status(200).json({ restaurants })
-    }
-    catch(err) {
-        next(err)
-    }
-}
-
-exports.filterRestaurantsByRatings = async(req, res, next) => {
-    try {
-        const { ratings } = req.body
-        let range = []
-        for(let rating in ratings){
-            range.push({ averageRating: { $gte: rating }})
-        }
-        const query = range.length ? { $or: range } : {};
-        const restaurants = await Restaurant.find(query)
-        res.status(200).json({ restaurants })
-    }
-    catch(err){
-        console.log(err)
-        next(err)
-    }
-}
-
-exports.filterRestaurantsByCostForTwo = async(req, res, next) => {
-    try {
-        const { costForTwo } = req.body
-        let range = []
-        if(costForTwo.includes("Less than 300")){
-            range.push({ costForTwo: { $lt: 300 }})
-        }
-        if(costForTwo.includes("Between 300-600")){
-            range.push({ costForTwo: { $gte: 300, $lte: 600 }})
-        }
-        if(costForTwo.includes("Greater than 600")){
-            range.push({ costForTwo: { $gt: 600 }})
-        }
-        const query = range.length ? { $or: range } : {};
-        const restaurants = await Restaurant.find(query)
-        res.status(200).json({ restaurants })
-    }
-    catch(err){
-        console.log(err)
-        next(err)
-    }
-}
 
 exports.updateRestaurant = async(req, res, next) => {
     try {
