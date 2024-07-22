973c1369b728c5a00d5e02a76c9af911da7fad2b
Vidhi <mathurvidhi2505@gmail.com>
2024-07-22 21:08:40 +0530

    Adjusted export setting and shadow

--- frontend/src/components/UI/Card.js
+++ frontend/src/components/UI/Card.js
@@ -1,7 +1,5 @@
-const Card = ({ children, className }) => {
+export const Card = ({ children, className }) => {
     return (
-        <div className={`max-w-md mx-auto bg-white rounded-lg shadow-md mt-8 ${className}`}>{children}</div>
+        <div className={`max-w-md mx-auto bg-white rounded-lg shadow-lg mt-8 ${className}`}>{children}</div>
     );
 }
-
-export default Card;
