d1f5a4a2c1a976f5e313d88a9e191fabd9945e13
Vidhi <mathurvidhi2505@gmail.com>
2024-07-23 12:22:27 +0530

    Changed check for restaurant and itemId to string from mongoId

--- backend/validators/user-related/cart-validator.js
+++ backend/validators/user-related/cart-validator.js
@@ -4,7 +4,7 @@ const { handleValidationErrors } = require("../common/handleValidationErrors")
 exports.updateCartValidation = [
     body('updates').notEmpty().withMessage("Updates can't be empty").bail().isArray().withMessage("Updates must be and array"),
     body("updates.*.type").isIn(['add', 'remove', 'clear']).withMessage("Invalid update type"),
-    body("updates.*.itemId").isMongoId().withMessage("Invalid ItemId"),
-    body('restaurant').isMongoId().withMessage("Invalid RestaurantId"),
+    body("updates.*.itemId").optional().isString().withMessage("Invalid ItemId"),
+    body('restaurant').isString().withMessage("Invalid RestaurantId"),
     handleValidationErrors
 ]
