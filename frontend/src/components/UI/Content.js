b88f19881e2ac2e625d742179af575fab6063706
Vidhi <mathurvidhi2505@gmail.com>
2024-07-25 11:58:59 +0530

    Modified max. width

--- frontend/src/components/UI/Content.js
+++ frontend/src/components/UI/Content.js
@@ -1,7 +1,7 @@
 export const Content = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col py-8">
-      <div className={`bg-white shadow-md rounded-lg p-6 w-full max-w-4xl mx-auto`}>{children}</div>
+      <div className={`bg-white shadow-md rounded-lg p-6 w-full max-w-[1100px] mx-auto`}>{children}</div>
     </div>
   );
 }
