0889337e42c868844365d7a67d9e255cd6387fba
Vidhi <mathurvidhi2505@gmail.com>
2024-07-25 12:00:08 +0530

    Adjusted styling to include additional details

--- frontend/src/components/Pages/RestaurantDetailPage.js
+++ frontend/src/components/Pages/RestaurantDetailPage.js
@@ -6,6 +6,7 @@ import { Menu } from "../restaurant-related/Menu";
 import { Review } from "../restaurant-related/Review";
 import { Photos } from "../restaurant-related/Photos";
 import { ErrorDialog } from "../UI/ErrorDialog";
+import { AccessTime, Phone, Email, LocationOn, Restaurant, CurrencyRupee, Star } from '@mui/icons-material';
 
 const RestaurantDetailPage = () => {
     const { restaurantId } = useParams();
@@ -43,70 +44,99 @@ const RestaurantDetailPage = () => {
     return (
         <Layout>
             <Content>
-                {loading && <div>Loading....</div>}
+                {loading && (
+                    <div className="flex justify-center items-center h-screen">
+                        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+                    </div>
+                )}
                 {errors && <ErrorDialog errors={errors} onClose={closeErrorDialogHandler}/>}
+                {!restaurantDetail && <div className="text-xl font-semibold">No restaurant details found.</div>}
                 {restaurantDetail && (
-                    <div className="max-w-4xl mx-auto px-4 py-8">
-                    <h1 className="text-3xl font-bold mb-4">{restaurantDetail.restaurantName}</h1>
-                    {/* Restaurant Images */}
-                    {restaurantDetail.images && restaurantDetail.images.length > 0 && (
+                    <div className="max-w-7xl mx-auto px-4 py-8">
                         <div className="mb-8">
-                            <h2 className="text-xl font-semibold mb-2">Images</h2>
-                            <div className="flex space-x-4 overflow-x-auto">
-                                {restaurantDetail.images.map((image, index) => (
-                                    <img key={index} alt={`${image}`} src={image} className="h-48 w-auto object-cover rounded-md" />
-                                ))}
+                            <h1 className="text-3xl font-bold mb-4">{restaurantDetail.restaurantName}</h1>
+                            <div className="flex items-center space-x-4 text-gray-400 font-semibold">
+                                <span className="flex items-center">
+                                    <Star className="w-5 h-5 text-yellow-400 mr-1" />
+                                    {restaurantDetail.averageRating.toFixed(1)}
+                                </span>
+                                <span>•</span>
+                                <span>{restaurantDetail.cuisine.join(", ")}</span>
+                                <span>•</span>
+                                <span className="flex items-center">
+                                    <CurrencyRupee className="w-5 h-5 mr-1" />
+                                    {restaurantDetail.costForTwo} for two
+                                </span>
                             </div>
                         </div>
-                    )}
-    
-                    <div className="flex">
-                        {/* Left Column */}
-                        <div className="w-3/4 mr-4">
-                            {/* Overview, Reviews, Menu */}
-                            <div className="mb-8">
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                            <div className="md:col-span-2">
+                                <div className="bg-white shadow-lg p-6">
+                                    <div className="flex space-x-4 border-b-2 border-gray-200">
+                                        <button onClick={() => setActive('menu')} className={`pb-2 pt-2 px-4 text-lg font-semibold ${active === 'menu'? 'border-b-2 border-orange-600 text-orange-600': 'text-gray-600'}`}>
+                                        Menu
+                                        </button>
+                                        <button onClick={() => setActive('reviews')} className={`pb-2 pt-2 px-4 text-lg font-semibold ${active === 'reviews'? 'border-b-2 border-orange-600 text-orange-600': 'text-gray-600'}`}>
+                                        Reviews
+                                        </button>
+                                        <button onClick={() => setActive('photos')} className={`pb-2 pt-2 px-4 text-lg font-semibold ${active === 'photos'? 'border-b-2 border-orange-600 text-orange-600': 'text-gray-600'}`}>
+                                        Photos
+                                        </button>
+                                    </div>
+                                    <div>
+                                        {active === 'menu' && <Menu menuId={restaurantDetail.menu} />}
+                                        {active === 'reviews' && <Review restaurantId={restaurantId} />}
+                                        {active === 'photos' && <Photos images={restaurantDetail.imageUrls}/>}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="bg-white shadow-lg p-6">
                                 <h2 className="text-xl font-semibold mb-2">Overview</h2>
-                                <div>
-                                    <p className="text-gray-700 mb-2"><span className="font-semibold">Address:</span> {restaurantDetail.address.street}, {restaurantDetail.address.city}, {restaurantDetail.address.state}, {restaurantDetail.address.postalCode}</p>
-                                    <p className="text-gray-700 mb-2"><span className="font-semibold">Opening Hours:</span></p>
-                                    <ul>
-                                    {restaurantDetail?.workingDays?.map((day) => (
-                                                <li key={day}>{day}: {restaurantDetail?.openingTime} - {restaurantDetail?.closingTime}</li>
-                                    ))}
-                                    </ul>
+                                <div className="space-y-4">
+                                    <div className="flex items-start">
+                                        <LocationOn className="w-5 h-5 text-gray-500 mr-2 mt-1" />
+                                        <div>
+                                            <h3 className="font-semibold">Phone Number</h3>
+                                            <p className="text-gray-600">{restaurantDetail.phone}</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-start">
+                                        <Phone className="w-5 h-5 text-gray-500 mr-2" />
+                                        <div>
+                                            <h3 className="font-semibold">Address</h3>
+                                            <p className="text-gray-600">
+                                                {restaurantDetail.address.street}, {restaurantDetail.address.city}, {restaurantDetail.address.state}, {restaurantDetail.address.postalCode}
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-start">
+                                        <Email className="w-5 h-5 text-gray-500 mr-2" />
+                                        <div>
+                                            <h3 className="font-semibold">Email</h3>
+                                            <p className="text-gray-600">{restaurantDetail.email}</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-start">
+                                        <AccessTime className="w-5 h-5 text-gray-500 mr-2" />
+                                        <div>
+                                            <h3 className="font-semibold">Opening Hours</h3>
+                                            <p className="text-gray-600">
+                                                {restaurantDetail.workingDays.join(', ')}<br />
+                                                {restaurantDetail.openingTime} - {restaurantDetail.closingTime}
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-start">
+                                        <Restaurant className="w-5 h-5 text-gray-500 mr-2" />
+                                        <div>
+                                            <h3 className="font-semibold">Food served</h3>
+                                            <p className="text-gray-600">{restaurantDetail.foodType}</p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-    
-                            <div className="mb-8">
-                                <div className="flex space-x-4 border-b-2 border-gray-200">
-                                    <button onClick={() => setActive('menu')} className={`pb-2 pt-2 px-4 text-lg font-semibold ${active === 'menu'? 'border-b-2 border-orange-600 text-orange-600': 'text-gray-600'}`}>
-                                    Menu
-                                    </button>
-                                    <button onClick={() => setActive('reviews')} className={`pb-2 pt-2 px-4 text-lg font-semibold ${active === 'reviews'? 'border-b-2 border-orange-600 text-orange-600': 'text-gray-600'}`}>
-                                    Reviews
-                                    </button>
-                                    <button onClick={() => setActive('photos')} className={`pb-2 pt-2 px-4 text-lg font-semibold ${active === 'photos'? 'border-b-2 border-orange-600 text-orange-600': 'text-gray-600'}`}>
-                                    Photos
-                                    </button>
-                                </div>
-                                <div>
-                                    {active === 'menu' && <Menu menuId={restaurantDetail.menu} />}
-                                    {active === 'reviews' && <Review restaurantId={restaurantId} />}
-                                    {active === 'photos' && <Photos images={restaurantDetail.imageUrls}/>}
-                                </div>
-                            </div>
-                        </div>
-    
-                        {/* Right Column */}
-                        <div className="w-1/4">
-                            {/* Phone Number */}
-                            <div className="mb-8">
-                                <h2 className="text-xl font-semibold mb-2">Contact</h2>
-                                <p className="text-gray-700">{restaurantDetail.phone}</p>
                             </div>
                         </div>
                     </div>
-                </div>
                 )}
             </Content>
         </Layout>
