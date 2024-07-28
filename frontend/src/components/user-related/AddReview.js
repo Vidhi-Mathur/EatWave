3d851940af85c44d65c8b2204fffeae8c02cb722
Vidhi <mathurvidhi2505@gmail.com>
2024-07-28 12:21:53 +0530

    Fixed editing and adding review issue

--- frontend/src/components/user-related/AddReview.js
+++ frontend/src/components/user-related/AddReview.js
@@ -4,7 +4,7 @@ import { AuthContext } from "../../store/Auth-Context";
 import { ErrorDialog } from "../UI/ErrorDialog";
 import { deleteImageHandler, uploadImageHandler } from "../../services/ImageService";
 
-export const AddReview = forwardRef(({orderId, restaurantId, existingReview, updateReview}, ref) => {
+export const AddReview = forwardRef(({orderId, restaurantId, existingReview, updateReview, onClose}, ref) => {
     const { token, fetchRefreshToken } = useContext(AuthContext);
     const [ addReview, setAddReview ] = useState(existingReview || {})
     const [ editReview, setEditReview ] = useState(!existingReview)
@@ -12,7 +12,8 @@ export const AddReview = forwardRef(({orderId, restaurantId, existingReview, upd
     const [ errors, setErrors ] = useState(null)
 
     useEffect(() => {
-        if(existingReview) setAddReview(existingReview)
+        setAddReview(existingReview || {})
+        setEditReview(!existingReview)
     }, [existingReview])
 
     const changeHandler = (e, newRating) => {
@@ -37,11 +38,11 @@ export const AddReview = forwardRef(({orderId, restaurantId, existingReview, upd
                     imageUrl = await uploadImageHandler([selectedFile], folder, token)
                     imageUrl = imageUrl[0]
                     if(previousImageUrl){
-                        await deleteImageHandler(previousImageUrl, token);
+                        await deleteImageHandler(previousImageUrl, folder, token);
                     }
                 }
                 catch(err) {
-                    console.log(err)
+                    setErrors([err.message || "Failed uploading image, try again later"])
                     return
                 }
             }
@@ -53,47 +54,51 @@ export const AddReview = forwardRef(({orderId, restaurantId, existingReview, upd
             }
             let url = `https://eatwave-api.onrender.com/review/${existingReview? existingReview._id: orderId}`
             let method = existingReview? 'PATCH': 'POST'
-            let response = await fetch(url, {
-                method,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': token? `Bearer ${token}`: ''
-                },
-                body: JSON.stringify(reviews)
-            })
-            const result = await response.json()
-            if(!response.ok){
-                if(response.status === 401){
-                   const refreshResponse = await fetchRefreshToken()
-                        if(refreshResponse){
-                            response = await fetch(url, {
-                            method,
-                            headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${refreshResponse.accessToken}`
-                            },
-                            body: JSON.stringify(reviews)
-                            })
-                            if(!response.ok){
-                                const errorMessages = result.errors ? result.errors.map(err => err.msg) : [result.message];
-                                setErrors(errorMessages);
-                                return;                   }
+            try {
+                let response = await fetch(url, {
+                    method,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': token? `Bearer ${token}`: ''
+                    },
+                    body: JSON.stringify(reviews)
+                })
+                const result = await response.json()
+                if(!response.ok){
+                    if(response.status === 401){
+                       const refreshResponse = await fetchRefreshToken()
+                            if(refreshResponse){
+                                response = await fetch(url, {
+                                method,
+                                headers: {
+                                'Content-Type': 'application/json',
+                                'Authorization': `Bearer ${refreshResponse.accessToken}`
+                                },
+                                body: JSON.stringify(reviews)
+                                })
+                                if(!response.ok){
+                                    const errorMessages = result.errors ? result.errors.map(err => err.msg) : [result.message];
+                                    setErrors(errorMessages);
+                                    return;                   
+                                }
+                                else {
+                                    setErrors(["Session expired, try login"])
+                                }
+                            }
                             else {
-                                setErrors(["Session expired, try login"])
+                                setErrors(["Can't save review, try again later"])
                             }
                         }
-                        else {
-                            setErrors(["Can't save review, try again later"])
-                        }
-                    }
-                setAddReview(result.review)
-                updateReview(orderId, result.review)
-                setEditReview(false)
+                    setAddReview(result.review)
+                    updateReview(orderId, result.review)
+                    setEditReview(false)
+                }
             }
-            setAddReview(existingReview || {})
-            setEditReview(!existingReview)
+            catch(err){
+                setErrors([err.message || "Failed saving review, try again later"])
+            }
         }
-        ref.current.close()
+        onClose()
     }
 
     const closeErrorDialogHandler = () => {
@@ -105,14 +110,18 @@ export const AddReview = forwardRef(({orderId, restaurantId, existingReview, upd
         {errors && <ErrorDialog errors={errors} onClose={closeErrorDialogHandler}/>}
         <div className="flex items-center justify-center">
             <div className="bg-white rounded-lg p-8">
-                <Rating name="rating" value={addReview.rating || 0} onChange={(e, newRating) => changeHandler(e, newRating)} readOnly={editReview}/>
-                <textarea id="comments" type="text" name="comments" className="border p-2 w-full mb-4" placeholder="comments" onChange={changeHandler} value={addReview.comments || ""} readOnly={editReview}/>
-                {!editReview? <input type="file" name="reviewImage" accept='image/*' className="border p-2 w-full mb-4" onChange={changeHandler} />: <img src={addReview.imageUrl} alt="Review" className="w-full h-auto max-h-64 object-contain rounded-lg mb-4" />}
+                <Rating name="rating" value={addReview.rating || 0} onChange={(e, newRating) => changeHandler(e, newRating)} readOnly={!editReview}/>
+                <textarea id="comments" type="text" name="comments" className="border p-2 w-full mb-4" placeholder="comments" onChange={changeHandler} value={addReview.comments || ""} readOnly={!editReview}/>
+                {editReview? (
+                    <input type="file" name="reviewImage" accept='image/*' className="border p-2 w-full mb-4" onChange={changeHandler} />
+                ): addReview.imageUrl && (
+                    <img src={addReview.imageUrl} alt="Review" className="w-full h-auto max-h-64 object-contain rounded-lg mb-4" />
+                )}
                 <div className="flex justify-end mt-8">
-                {!editReview? (
-                <button type="button" className="bg-green-500 text-white py-2 px-4 rounded mr-4" onClick={() => modalHandler(true)}>Save</button>
+                {editReview? (
+                    <button type="button" className="bg-green-500 text-white py-2 px-4 rounded mr-4" onClick={() => modalHandler(true)}>Save</button>
                 ): (
-                    <button type="button" className="bg-orange-500 text-white py-2 px-4 rounded mr-4" onClick={() => setEditReview(false)}>Edit</button>
+                    <button type="button" className="bg-orange-500 text-white py-2 px-4 rounded mr-4" onClick={() => setEditReview(true)}>Edit</button>
                 )}
                 <button type="button" className="bg-red-500 text-white py-2 px-4 rounded" onClick={() => modalHandler(false)}>Cancel</button>
                 </div>
