e91a68cf641fca94227a1c932642ec2497d71790
Vidhi <mathurvidhi2505@gmail.com>
2024-07-28 12:21:10 +0530

    Fixed editing and adding review issue

--- frontend/src/components/user-related/PastOrders.js
+++ frontend/src/components/user-related/PastOrders.js
@@ -20,13 +20,19 @@ const formattedDate = (dateString) => {
 export const PastOrders = () => {
     const { token, fetchRefreshToken } = useContext(AuthContext);
     const [ orders, setOrders ] = useState(null);
-    const [ review, setReview ] = useState({})
+    const [ reviews, setReviews ] = useState({})
+    const [ currentOrderId, setCurrentOrderId ] = useState(null)
     const [ errors, setErrors ] = useState(null)
     const dialog = useRef()
 
     const openModalHandler = (orderId) => {
+        setCurrentOrderId(orderId)
         dialog.current.showModal()
-        setReview(prev => ({...prev, currentOrder: orderId}))
+    }
+
+    const closeModalHandler = () => {
+        dialog.current.close()
+        setCurrentOrderId(null)
     }
 
     const fetchReviewByOrderId = async(orderId) => {
@@ -62,11 +68,11 @@ export const PastOrders = () => {
                     setErrors(["Can't fetch reviews, try again later"])
                 }
             }
-            setReview(prev => ({...prev, [orderId]: result.review[0]}));
+            setReviews(prev => ({...prev, [orderId]: result.review[0]}));
             return result
         } 
         catch (err) {
-            setErrors([err || "Failed fetching reviews, try again later"])
+            setErrors([err.message || "Failed fetching reviews, try again later"])
         }
     }
 
@@ -116,7 +122,7 @@ export const PastOrders = () => {
     }, [fetchRefreshToken, token]);
 
     const updateReviewHandler = (orderId, updatedReview) => {
-        setReview(prev => ({ ...prev, [orderId]: updatedReview }));
+        setReviews(prev => ({ ...prev, [orderId]: updatedReview }));
     };
 
     const closeErrorDialogHandler = () => {
@@ -128,7 +134,10 @@ export const PastOrders = () => {
             <h1 className="text-2xl font-bold mb-4">Past Orders</h1>
             {errors && <ErrorDialog errors={errors} onClose={closeErrorDialogHandler} />}
             <ul className="space-y-4">
-                {orders === null && <p>Loading...</p>}
+                {orders === null && (
+                <div className="flex justify-center items-center h-screen">
+                    <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+                </div>)}
                 {orders !== null && orders.length === 0 && <p>No orders placed yet</p>}
                 {orders !== null && orders.length > 0 && orders.map(order => (
                     <li key={order._id} className="border rounded p-4 shadow">
@@ -136,11 +145,11 @@ export const PastOrders = () => {
                         <p className="text-gray-700">from {order.restaurant.restaurantName}</p>
                         <p className="text-gray-700">Ordered on {formattedDate(order.createdAt)}</p>
                         <p className="text-gray-700">Total Paid: &#8377; {order.totalCost}</p>
-                        {/* <button type="button" className="mt-3 bg-orange-500 text-white py-2 px-3 rounded" onClick={() => openModalHandler(order._id)}>{review[order._id]? "View Review": "Save Review"}</button> */}
-                        <AddReview ref={dialog} orderId={order._id} restaurantId={order.restaurant._id} existingReview={review[order._id]} updateReview={updateReviewHandler}/>
+                        <button type="button" className="mt-3 bg-orange-500 text-white py-2 px-3 rounded" onClick={() => openModalHandler(order._id)}>{reviews[order._id]? "View Review": "Save Review"}</button>
                     </li>
                 ))}
             </ul>
+            <AddReview ref={dialog} orderId={currentOrderId} restaurantId={orders?.find(order => order._id === currentOrderId)?.restaurant._id} existingReview={reviews[currentOrderId]} updateReview={updateReviewHandler} onClose={closeModalHandler} />
         </div>
     );
 };
