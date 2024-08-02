7d512e9e49297c83ae3ba6fc827d33b362deaefb
Vidhi <mathurvidhi2505@gmail.com>
2024-08-02 20:04:45 +0530

    Modified styling

--- frontend/src/components/Pages/HomePage.js
+++ frontend/src/components/Pages/HomePage.js
@@ -7,14 +7,13 @@ let TopPicks = React.lazy(() => import("../restaurant-related/TopPicks"))
 
 const HomePage = () => {
 const [restaurants, setRestaurants] = useState([])
-
   return (
     <Layout>
       <Suspense fallback="Loading">
         <TopPicks />
       </Suspense>
       <div>
-        <div className="inline-flex items-center space-x-4">
+        <div className="lg:inline-flex items-center space-x-4 sm:flex md:flex">
           <h1 className="ml-10 text-3xl font-bold text-white bg-gray-800 bg-opacity-75 px-4 py-2 rounded-lg shadow-lg">Choose what you want </h1>
           <FilterDialog setRestaurants = {setRestaurants} />
         </div>
