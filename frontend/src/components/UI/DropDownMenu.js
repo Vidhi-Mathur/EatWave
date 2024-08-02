fac43015ea82cb9c0f6b98471a807277209e2480
Vidhi <mathurvidhi2505@gmail.com>
2024-08-02 20:09:04 +0530

    Displays user's name on login or signup

--- frontend/src/components/UI/DropDownMenu.js
+++ frontend/src/components/UI/DropDownMenu.js
@@ -4,7 +4,7 @@ import { AuthContext } from "../../store/Auth-Context"
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export const DropDownMenu = () => {
-    const { logout } = useContext(AuthContext)
+    const { logout, details } = useContext(AuthContext)
     const [dropDown, setDropDown] = useState(false)
     
     const openHandler = () => {
@@ -23,9 +23,11 @@ export const DropDownMenu = () => {
     return (
         <>
         <div className="relative inline-block" onMouseEnter={openHandler} onMouseLeave={closeHandler}>
-                <NavLink to="/my-account" className="mr-4 md:mr-16"><AccountCircleIcon /> Username</NavLink>
+                <NavLink to="/my-account" className="mr-4 md:mr-16 flex items-center"><AccountCircleIcon /> <span className="ml-1 truncate max-w-[100px]">{details.name}</span>
+                </NavLink>
                 {dropDown && (
-                    <div className="z-10 absolute bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
+                    <div className="z-10 absolute right-0 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 mt-2" onMouseEnter={openHandler} onMouseLeave={closeHandler}>
+                    <div className="absolute right-20 -top-2 w-0 h-0 border-l-8 border-r-8 border-b-8 border-transparent border-b-white"></div>
                     <nav className="py-2 text-sm text-gray-700">
                     <NavLink to='/my-account' className="block px-4 py-2 hover:bg-gray-100">Profile</NavLink>
                     <NavLink to='/my-account/orders' className="block px-4 py-2 hover:bg-gray-100">Orders</NavLink>
