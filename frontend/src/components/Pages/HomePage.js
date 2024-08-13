3e517709cde789af7cd13f8d61fe924a02f97e9f
Vidhi <mathurvidhi2505@gmail.com>
2024-08-13 22:59:04 +0530

    Removed lazy-loading on TopPicks component

--- frontend/src/components/Pages/HomePage.js
+++ frontend/src/components/Pages/HomePage.js
@@ -1,17 +1,15 @@
-import React, { Suspense, useState } from "react";
+import React, { useState } from "react";
 import { Layout }from "../UI/Layout";
 import { SortMenu } from "../UI/SortMenu";
 import { FilterDialog } from "../UI/FilterDialog";
 import { SortedRestaurants } from "../restaurant-related/SortedRestaurants";
-let TopPicks = React.lazy(() => import("../restaurant-related/TopPicks"))
+import TopPicks from "../restaurant-related/TopPicks"
 
 export const HomePage = () => {
 const [restaurants, setRestaurants] = useState([])
   return (
     <Layout>
-      <Suspense fallback="Loading">
         <TopPicks />
-      </Suspense>
       <div>
         <div className="lg:inline-flex items-center space-x-4 sm:flex md:flex">
           <h1 className="ml-10 text-3xl font-bold text-white bg-gray-800 bg-opacity-75 px-4 py-2 rounded-lg shadow-lg">Choose what you want </h1>
