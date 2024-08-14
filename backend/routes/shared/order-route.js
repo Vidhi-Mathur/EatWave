8a53b0ade4228c0b14dab0b4db589b957640d8bc
Vidhi <mathurvidhi2505@gmail.com>
2024-08-14 18:25:28 +0530

    Removed unecessary route

--- backend/routes/shared/order-route.js
+++ backend/routes/shared/order-route.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { initiatePayment, placeOrder, getUserOrderHistory, getOrderByOrderId, updateOrderStatus, getRestaurantOrderHistory, cancelOrder} = require('../../controllers/shared/order-controller')
-const { initiatePaymentValidation, placeOrderValidation, getOrderByOrderIdValidation, getRestaurantOrderHistoryValidation} = require("../../validators/shared/order-validator")
-const {authorizationMiddleware } = require('../../controllers/user-related/authentication-controller')
+const { initiatePayment, placeOrder, getUserOrderHistory, getOrderByOrderId, getRestaurantOrderHistory } = require('../../controllers/shared/order-controller')
+const { initiatePaymentValidation, placeOrderValidation, getOrderByOrderIdValidation, getRestaurantOrderHistoryValidation
+} = require("../../validators/shared/order-validator")
+const { authorizationMiddleware } = require('../../controllers/user-related/authentication-controller')
 
 router.use(authorizationMiddleware)
 
@@ -18,13 +19,8 @@ router.get('/user_history', getUserOrderHistory)
 //GET /eatwave/order/:orderId
 router.get('/:orderId', getOrderByOrderIdValidation, getOrderByOrderId)
 
-//PATCH /eatwave/order/status/:orderId
-router.patch('/status/:orderId', updateOrderStatus)
-
-//GET /eavwave/order/restaurant_history/:restaurantId
+//GET /eatwave/order/restaurant_history/:restaurantId
 router.get('/restaurant_history/:restaurantId', getRestaurantOrderHistoryValidation, getRestaurantOrderHistory)
 
-//DELETE /eatwave/cancel/:id
-router.delete('/cancel/:orderId', cancelOrder)
 
 module.exports = router
