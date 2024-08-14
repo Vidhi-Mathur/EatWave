6fcca91aa8cedeb5bb8581c0a3a7ce1cc023d7bf
Vidhi <mathurvidhi2505@gmail.com>
2024-08-14 18:20:11 +0530

    Added track Order Link and modified loading state

--- frontend/src/components/user-related/PastOrders.js
+++ frontend/src/components/user-related/PastOrders.js
@@ -3,6 +3,7 @@ import { AuthContext } from "../../store/Auth-Context";
 import { AddReview } from "./AddReview";
 import { ErrorDialog } from "../UI/ErrorDialog";
 import { LoadingSpinner } from "../UI/LoadingSpinner";
+import { Link } from "react-router-dom";
 
 const formattedDate = (dateString) => {
     const options = { 
@@ -24,7 +25,7 @@ export const PastOrders = () => {
     const [ reviews, setReviews ] = useState({})
     const [ currentOrderId, setCurrentOrderId ] = useState(null)
     const [ errors, setErrors ] = useState(null)
-    const [loading, setLoading] = useState(true);
+    const [ loading, setLoading] = useState(true);
     const dialog = useRef()
 
     const openModalHandler = (orderId) => {
@@ -36,10 +37,10 @@ export const PastOrders = () => {
         dialog.current.close()
         setCurrentOrderId(null)
     }
-
+    
     const fetchReviewByOrderId = async(orderId) => {
         try {
-            let response = await fetch(`${process.env.REACT_APP_API_URL}/review/order/${orderId}`, {
+            let response = await fetch(`${process.env.SERVER_URL}/review/order/${orderId}`, {
                 headers: {
                     'Authorization': token ? `Bearer ${token}` : ' '
                 }
@@ -49,7 +50,7 @@ export const PastOrders = () => {
                 if(response.status === 401){
                     const refreshResponse = await fetchRefreshToken()
                     if(refreshResponse){
-                        response = await fetch(`${process.env.REACT_APP_API_URL}/review/order/${orderId}`, {
+                        response = await fetch(`${process.env.SERVER_URL}/review/order/${orderId}`, {
                             headers: {
                                 'Authorization': `Bearer ${refreshResponse.accessToken}`
                             }
@@ -86,7 +87,7 @@ export const PastOrders = () => {
     useEffect(() => {
         const fetchPastOrders = async () => {
             try {
-                let response = await fetch(`${process.env.REACT_APP_API_URL}/order/user_history`, {
+                let response = await fetch(`${process.env.SERVER_URL}/order/user_history`, {
                     headers: {
                         'Authorization': token ? `Bearer ${token}` : ' '
                     }
@@ -96,29 +97,28 @@ export const PastOrders = () => {
                     if(response.status === 401){
                         const refreshResponse = await fetchRefreshToken()
                         if(refreshResponse){
-                            response = await fetch(`${process.env.REACT_APP_API_URL}/order/user_history`, {
+                            response = await fetch(`${process.env.SERVER_URL}/order/user_history`, {
                                 headers: {
                                     'Authorization': `Bearer ${refreshResponse.accessToken}`
                                 }
                             });
                             if(!response.ok) {
-                                setErrors(["Can't fetch past orders, try again later"])
+                                setErrors([result.message || "Can't fetch past orders, try again later"])
+                                setLoading(false);
                                 return
                             }
                         }
                         else {
-                            setErrors(["Session expired, try again later"])
+                            setErrors([result.message || "Session expired, try again later"])
+                            setLoading(false);
                             return
                         }
                     }
-                    else if(result.errors){
-                        const errorMessages = result.errors.map((err) => err.msg)
+                    else{
+                        const errorMessages = result.errors ? result.errors.map(err => err.msg) : [result.message];
                         setErrors(errorMessages);
+                        setLoading(false);
                         return 
-                    }
-                    else {
-                        setErrors(["Can't fetch past orders, try again later"])
-                        return
                     }
                 }
                 setOrders(result.orders);
@@ -156,7 +156,10 @@ export const PastOrders = () => {
                         <p className="text-gray-700">from {order.restaurant.restaurantName}</p>
                         <p className="text-gray-700">Ordered on {formattedDate(order.createdAt)}</p>
                         <p className="text-gray-700">Total Paid: &#8377; {order.totalCost}</p>
-                        <button type="button" className="mt-3 bg-orange-500 text-white py-2 px-3 rounded" onClick={() => openModalHandler(order._id)}>{reviews[order._id]? "View Review": "Save Review"}</button>
+                        <button type="button" className="mt-3 bg-orange-500 text-white py-2 px-3 rounded hover:bg-white hover:text-orange-500 hover:border hover:border-orange-500" onClick={() => openModalHandler(order._id)}>{reviews[order._id]? "View Review": "Save Review"}</button>
+                        <Link to={`/my-account/orders/track-order/${order._id}`}>
+                        <button className="ml-2 mt-3 bg-white text-orange-500 border border-orange-500 py-2 px-3 rounded hover:bg-orange-500 hover:text-white hover:border-white">Track Order</button>
+                        </Link>
                     </li>
                 ))}
             </ul>
